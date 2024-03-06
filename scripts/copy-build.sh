7d0f8ed2263fedc8a860618d39ecf5bb66a692db
Niek Kruse <niek@framer.com>
2024-03-06 14:40:00 +0100

    rm echo

--- scripts/copy-build.sh
+++ scripts/copy-build.sh
@@ -6,11 +6,9 @@
 # Iterate over all directories in ./plugins
 for d in ./plugins/*/; do
     # Check if a dist directory exists for the current plugin
-    echo ${d}
     if [ -d "${d}dist" ]; then
         # Extract the name of the plugin directory
         plugin_dir_name=$(basename "${d}")
-        echo $plugin_dir_name
         # Create plugin directory in p if it does not exist
         [ ! -d "./p/${plugin_dir_name}" ] && mkdir -p "./p/${plugin_dir_name}"
         # Copy all files from the dist directory to the target directory

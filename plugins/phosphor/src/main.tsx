2585d0d84830ea629d2e75f30727e30b90026f90
Hunter Caron <huntercaron@me.com>
2024-04-12 16:09:19 +0200

    Updating height

--- plugins/phosphor/src/main.tsx
+++ plugins/phosphor/src/main.tsx
@@ -1,28 +1,28 @@
-import "./globals.css";
+import "./globals.css"
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
-import { framer } from "framer-plugin";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App.tsx"
+import { framer } from "framer-plugin"
 
-const root = document.getElementById("root");
+const root = document.getElementById("root")
 if (!root) {
-  throw new Error("Root element not found");
+  throw new Error("Root element not found")
 }
 
 import.meta.hot?.dispose(() => {
-  void framer.closePlugin();
-});
+  void framer.closePlugin()
+})
 
 void framer.showUI({
   position: "top left",
   width: 240,
-  height: 9000,
+  height: 445,
   resizable: true,
-});
+})
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)

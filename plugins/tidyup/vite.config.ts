1bd3beb769145ea4130457db6a5bfc895ba7afea
Jonas Treub <jonastreub@me.com>
2024-04-15 14:37:01 +0200

    Add Tidy Up

---
+++ plugins/tidyup/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import mkcert from "vite-plugin-mkcert"
+import framer from "vite-plugin-framer"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    server: { strictPort: true },
+    plugins: [react(), mkcert(), framer()],
+})

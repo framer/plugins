caa9beabfeed11acf14d98a9fcbfe94028ece192
Niek Kruse <niek@framer.com>
2024-05-07 11:36:10 +0200

    Cleanup and code comments

--- plugins/pong/src/main.tsx
+++ plugins/pong/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { App } from "./App.tsx"
 
+import { framer } from "framer-plugin"
 import "./globals.css"
 
 const root = document.getElementById("root")

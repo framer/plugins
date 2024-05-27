93d37eb370e573f70c2b52aee6335e77ae2ea2ac
Niek Kruse <niek@framer.com>
2024-05-27 21:27:48 +0200

    Add layout insert plugin

---
+++ plugins/layout-insert/src/main.tsx
@@ -0,0 +1,25 @@
+import "./globals.css"
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App.tsx"
+import { framer } from "framer-plugin"
+
+const root = document.getElementById("root")
+
+if (!root) throw new Error("Root element not found")
+
+root.style.height = "auto"
+const resizeObserver = new ResizeObserver(([element]) => {
+  framer.showUI({
+    height: element.contentRect.height,
+  })
+})
+
+resizeObserver.observe(root)
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

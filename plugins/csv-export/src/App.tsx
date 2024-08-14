fff59a8845927ccb81475a221ce53df3d17b82b1
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2024-08-14 10:00:36 +0100

    Add CSV export plugin example

---
+++ plugins/csv-export/src/App.tsx
@@ -0,0 +1,105 @@
+import type { CollectionItem } from "framer-plugin"
+
+import "./App.css"
+import { framer } from "framer-plugin"
+import { useEffect, useRef, useState } from "react"
+import { exportCollectionAsCSV, getDataForCSV } from "./csv"
+
+export function App() {
+    const container = useRef<HTMLDivElement>(null)
+    const content = useRef<HTMLTableElement>(null)
+
+    const [showGradient, setShowGradient] = useState(false)
+    const [collectionName, setCollectionName] = useState<string>()
+    const [previewCSV, setPreviewCSV] = useState<string[][]>()
+
+    useEffect(() => {
+        framer.showUI({
+            title: "Export",
+            width: 600,
+            height: 370,
+            resizable: "width",
+        })
+
+        const load = async () => {
+            const collection = await framer.getCollection()
+            if (!collection) return
+
+            const fields = await collection.getFields()
+            const items = await collection.getItems()
+            const previewItems: CollectionItem[] = []
+
+            for (let index = 0; index < items.length; index++) {
+                previewItems.push(items[index])
+            }
+
+            setPreviewCSV(getDataForCSV(fields, previewItems))
+            setCollectionName(collection.name)
+        }
+
+        const resize = () => {
+            if (!container.current || !content.current) return
+
+            const containerBounds = container.current.getBoundingClientRect()
+            const contentBounds = content.current.getBoundingClientRect()
+
+            setShowGradient(containerBounds.height - 25 < contentBounds.height)
+        }
+
+        window.addEventListener("resize", resize)
+
+        load()
+        resize()
+
+        return () => {
+            window.removeEventListener("resize", resize)
+        }
+    }, [])
+
+    const exportCSV = () => {
+        framer.getCollection().then(exportCollectionAsCSV)
+    }
+
+    if (!previewCSV?.length) return null
+
+    return (
+        <div className="export-collection">
+            <div className="table" ref={container}>
+                <div className="table-scrollable">
+                    <table ref={content}>
+                        <thead>
+                            <tr>
+                                {previewCSV[0].map((cell, columnIndex) => (
+                                    <td key={`0-${columnIndex}`}>{cell}</td>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {previewCSV.map((row, rowIndex) => {
+                                if (rowIndex === 0) return
+
+                                return (
+                                    <tr key={`${rowIndex}`}>
+                                        {row.map((cell, columnIndex) => (
+                                            <td key={`${rowIndex}-${columnIndex}`}>{cell}</td>
+                                        ))}
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+
+                {showGradient && <div className="gradient" />}
+                <div className="border" />
+            </div>
+
+            <div className="actions">
+                <input type="text" defaultValue={collectionName} placeholder="Filename" />
+                <button className="framer-button-primary" onClick={exportCSV}>
+                    Export as CSV
+                </button>
+            </div>
+        </div>
+    )
+}
---
+++ plugins/csv-export/src/csv.ts
@@ -0,0 +1,126 @@
+import type { Collection, CollectionField, CollectionItem } from "framer-plugin"
+
+import { isFileAsset, isImageAsset } from "framer-plugin"
+import { assertNever } from "framer-plugin/utils"
+
+function downloadFile(file: File) {
+    const filename = file.name
+    const fileURL = URL.createObjectURL(file)
+
+    const link = document.createElement("a")
+    link.href = fileURL
+    link.download = filename
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
+
+    URL.revokeObjectURL(fileURL)
+}
+
+type Columns = string[]
+type Rows = Columns[]
+
+/**
+ * Escape quotes with another quote as per CSV standard.
+ */
+function escapeCell(value: string) {
+    return value.replace(/"/gu, '""')
+}
+
+export function getDataForCSV(fields: CollectionField[], items: CollectionItem[]): Rows {
+    const rows: Rows = []
+
+    // Add header row.
+    rows.push(fields.map(field => field.name))
+
+    // Add slug header to the start.
+    rows[0].unshift("slug")
+
+    // Add all the data rows.
+    for (const item of items) {
+        const columns: Columns = []
+
+        // Add the slug cell.
+        columns.push(item.slug)
+
+        for (const field of fields) {
+            const value = item.fieldData[field.id]
+
+            switch (field.type) {
+                case "image":
+                case "file": {
+                    if (!isImageAsset(value) && !isFileAsset(value)) {
+                        columns.push("")
+                        continue
+                    }
+
+                    columns.push(`${value.url}`)
+                    continue
+                }
+
+                case "formattedText": {
+                    if (typeof value !== "string") {
+                        columns.push("")
+                        continue
+                    }
+
+                    columns.push(`${value}`)
+                    continue
+                }
+
+                case "enum": {
+                    if (!value) {
+                        columns.push(field.cases[0].name)
+                        continue
+                    }
+
+                    columns.push(`${value}`)
+                    continue
+                }
+
+                case "string":
+                case "boolean":
+                case "color":
+                case "date":
+                case "link":
+                case "number":
+                    columns.push(`${value}`)
+                    continue
+
+                case "unsupported":
+                    columns.push("")
+                    continue
+
+                default:
+                    assertNever(field)
+            }
+        }
+
+        rows.push(columns)
+    }
+
+    return rows
+}
+
+export async function exportCollectionAsCSV(collection: Collection) {
+    const fields = await collection.getFields()
+    const items = await collection.getItems()
+
+    const rows = getDataForCSV(fields, items)
+
+    const csv = rows
+        .map(row => {
+            return row
+                .map(column => {
+                    return `"${escapeCell(column)}"`
+                })
+                .join(", ")
+        })
+        .join("\n")
+
+    const file = new File([csv], `${collection.name}.csv`, {
+        type: "text/csv",
+    })
+
+    downloadFile(file)
+}

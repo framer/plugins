15f6fa4ea381f40efb534d54bf46aaae8e8b522d
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2024-09-17 12:45:31 +0100

    Use default port and endpoint for localhost worker

--- plugins/google-sheets/src/auth.ts
+++ plugins/google-sheets/src/auth.ts
@@ -26,7 +26,7 @@ class Auth {
 
     constructor() {
         this.AUTH_URI = location.hostname.includes("localhost")
-            ? "https://localhost:3001/auth"
+            ? "https://localhost:8787"
             : "https://framer-google-sheets-api.sakibulislam25800.workers.dev/auth"
     }
 

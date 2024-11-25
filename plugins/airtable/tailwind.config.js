9511d0759f11b0c4709d9f769e5cc335bbc0b088
Cédric Boirard <cedric@framer.com>
2024-11-25 16:49:08 +0100

    Fix selecting fields
    Signed-off-by: Cédric Boirard <cedric@framer.com>

--- plugins/airtable/tailwind.config.js
+++ plugins/airtable/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["selector", '[data-framer-theme="dark"]'],
     theme: {
         extend: {
             backgroundColor: {

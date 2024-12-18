311dfab8b4a467e53cd393cde13682116f17e1d5
Tom Watson <tom@framer.com>
2024-12-18 17:30:05 +0100

    Refactor to use new API shape

--- plugins/localization-import-export/eslint.config.js
+++ plugins/localization-import-export/eslint.config.js
@@ -20,6 +20,13 @@ export default tseslint.config(
         rules: {
             ...reactHooks.configs.recommended.rules,
             "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_$",
+                },
+            ],
         },
     }
 )

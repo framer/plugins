2e6a78fb2ae9df78943baa9a854080974e21fba0
Cédric Boirard <cedric@framer.com>
2024-12-24 14:15:35 +0100

    Extract DataSource selection and synchronization
    Signed-off-by: Cédric Boirard <cedric@framer.com>

--- examples/cms/src/main.tsx
+++ examples/cms/src/main.tsx
@@ -2,13 +2,33 @@ import "framer-plugin/framer.css"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { framer } from "framer-plugin"
 import { App } from "./App.tsx"
+import { PLUGIN_KEYS } from "./constants"
+import { syncExistingCollection } from "./data"
 
-const root = document.getElementById("root")
-if (!root) throw new Error("Root element not found")
+const activeCollection = await framer.getManagedCollection()
 
-ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+const configuredCollectionId = await activeCollection.getPluginData(PLUGIN_KEYS.COLLECTION_ID)
+const configuredSlugFieldId = await activeCollection.getPluginData(PLUGIN_KEYS.SLUG_FIELD_ID)
+
+const result = await syncExistingCollection(activeCollection, configuredCollectionId, configuredSlugFieldId)
+
+if (result === "success") {
+    await framer.closePlugin(`Synchronization successful`, {
+        variant: "success",
+    })
+} else {
+    const root = document.getElementById("root")
+    if (!root) throw new Error("Root element not found")
+
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App
+                collection={activeCollection}
+                dataSourceId={configuredCollectionId}
+                slugFieldId={configuredSlugFieldId}
+            />
+        </React.StrictMode>
+    )
+}

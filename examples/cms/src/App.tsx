d267877d2de109704f636b2770c16250904954f7
Cédric Boirard <cedric@framer.com>
2024-12-24 14:15:35 +0100

    Extract DataSource selection and synchronization
    Signed-off-by: Cédric Boirard <cedric@framer.com>

--- examples/cms/src/App.tsx
+++ examples/cms/src/App.tsx
@@ -1,69 +1,24 @@
-import { framer, ManagedCollectionField } from "framer-plugin"
 import "./App.css"
-import { useLayoutEffect, useState } from "react"
-import { DataSource, getDataSources, syncCollection, syncExistingCollection } from "./data"
+
+import { framer, ManagedCollection, ManagedCollectionField } from "framer-plugin"
+import { useEffect, useLayoutEffect, useState } from "react"
+import { DataSource, getDataSource, getDataSourcesIds, syncCollection } from "./data"
 import { FieldMapping } from "./FieldMapping"
-import { PLUGIN_KEYS, UI_DEFAULTS } from "./constants"
-import { Spinner } from "./components/Spinner"
+import { SelectDataSource } from "./SelectDataSource"
+import { UI_DEFAULTS } from "./constants"
 
-const activeCollection = await framer.getManagedCollection()
-
-const syncDataSourceId = await activeCollection.getPluginData(PLUGIN_KEYS.SYNC_REFERENCE)
-const syncSlugFieldId = await activeCollection.getPluginData(PLUGIN_KEYS.SYNC_SLUG)
-
-const result = await syncExistingCollection(activeCollection, syncDataSourceId, syncSlugFieldId)
-
-if (result.status === "success") {
-    await framer.closePlugin(`Synchronization successful`, {
-        variant: "success",
-    })
+interface AppProps {
+    collection: ManagedCollection
+    dataSourceId: string | null
+    slugFieldId: string | null
 }
 
-export function App() {
-    const [isLoadingFields, setIsLoadingFields] = useState(false)
-
-    const [selectedDataSourceId, setSelectedDataSourceId] = useState<string | null>(() => {
-        if (result.status === "needsSetup") {
-            return result.allDataSources[0] || null
-        }
-
-        return syncDataSourceId || null
-    })
-    const [selectedDataSource, setSelectedDataSource] = useState<DataSource | null>(
-        result.status === "needsConfiguration" ? result.dataSource : null
-    )
-    const [existingFields, setExistingFields] = useState<ManagedCollectionField[]>(
-        result.status === "needsConfiguration" ? result.existingFields : []
-    )
-
-    const showFieldsMapping = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-
-        if (!selectedDataSourceId) {
-            framer.notify("Please select a data source", { variant: "error" })
-            return
-        }
-
-        setIsLoadingFields(true)
-
-        try {
-            const dataSource = await getDataSources(selectedDataSourceId)
-            if (!dataSource) {
-                throw new Error("Failed to load data source")
-            }
-            setSelectedDataSource(dataSource)
-            setExistingFields(await activeCollection.getFields())
-        } catch (error) {
-            framer.notify(`Failed to load collection: ${error instanceof Error ? error.message : "Unknown error"}`, {
-                variant: "error",
-            })
-        } finally {
-            setIsLoadingFields(false)
-        }
-    }
+export function App({ collection, dataSourceId, slugFieldId }: AppProps) {
+    const [dataSource, setDataSource] = useState<DataSource | null>(null)
+    const [existingFields, setExistingFields] = useState<ManagedCollectionField[]>([])
 
     useLayoutEffect(() => {
-        if (!selectedDataSource) {
+        if (!dataSource) {
             framer.showUI({
                 width: UI_DEFAULTS.SETUP_WIDTH,
                 height: UI_DEFAULTS.SETUP_HEIGHT,
@@ -79,56 +34,31 @@ export function App() {
             minHeight: UI_DEFAULTS.MAPPING_HEIGHT,
             resizable: true,
         })
-    }, [selectedDataSource])
+    }, [dataSource])
 
-    if (result.status === "needsSetup" && !selectedDataSource) {
-        return (
-            <main className="setup">
-                <div className="logo">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150">
-                        <path
-                            fill="#999"
-                            d="M75 33c18.778 0 34 7.611 34 17S93.778 67 75 67s-34-7.611-34-17 15.222-17 34-17Zm34 40.333C109 82.538 93.778 90 75 90s-34-7.462-34-16.667V60c0 9.389 15.222 17 34 17 18.776 0 33.997-7.61 34-16.997v13.33ZM109 84v.497c0-.166-.005-.332-.015-.497Zm0 13.333C109 106.538 93.778 114 75 114s-34-7.462-34-16.667V84h.015c-.01.166-.015.333-.015.5 0 9.113 15.222 16.5 34 16.5 18.776 0 33.997-7.386 34-16.497v12.83Z"
-                        />
-                    </svg>
-                </div>
-                <form onSubmit={showFieldsMapping}>
-                    <label htmlFor="collection">
-                        Collection
-                        <select
-                            id="collection"
-                            onChange={event => setSelectedDataSourceId(event.target.value)}
-                            value={selectedDataSourceId || ""}
-                        >
-                            <option value="" disabled>
-                                Choose...
-                            </option>
-                            {result.allDataSources.map(collectionId => (
-                                <option key={collectionId} value={collectionId}>
-                                    {collectionId}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
-                    <button style={{ position: "relative" }} disabled={!selectedDataSourceId || isLoadingFields}>
-                        {isLoadingFields ? <Spinner inheritColor /> : "Next"}
-                    </button>
-                </form>
-            </main>
-        )
-    }
+    useEffect(() => {
+        collection.getFields().then(setExistingFields)
+    }, [collection])
 
-    if (selectedDataSource) {
+    useEffect(() => {
+        if (!dataSourceId) {
+            return
+        }
+
+        getDataSource(dataSourceId).then(setDataSource)
+    }, [dataSourceId])
+
+    if (!dataSource) {
+        return <SelectDataSource dataSources={getDataSourcesIds()} onSelectDataSource={setDataSource} />
+    } else {
         return (
             <FieldMapping
-                dataSource={selectedDataSource}
-                savedFieldsConfig={result.status === "needsConfiguration" ? result.savedFieldsConfig : null}
+                collection={collection}
+                dataSource={dataSource}
                 existingFields={existingFields}
-                savedSlugFieldId={syncSlugFieldId}
-                onSubmit={syncCollection}
+                slugFieldId={slugFieldId}
+                onImport={syncCollection}
             />
         )
     }
-
-    throw new Error(`Invalid state: ${result.status}`)
 }
--- examples/cms/src/data.ts
+++ examples/cms/src/data.ts
@@ -1,12 +1,15 @@
 import { CollectionItemData, ManagedCollection, ManagedCollectionField, framer } from "framer-plugin"
-import { generateHash, isNotNull } from "./utils"
 import { PLUGIN_KEYS } from "./constants"
 
 export type ManagedCollectionFieldType = ManagedCollectionField["type"]
 
-type DataSourceField = {
-    type: ManagedCollectionFieldType
-}
+type DataSourceField =
+    | {
+          type: ManagedCollectionFieldType
+      }
+    | {
+          type: "html"
+      }
 
 export interface DataSource {
     id: string
@@ -14,61 +17,60 @@ export interface DataSource {
     items: Record<string, unknown>[]
 }
 
-export interface FieldConfig {
-    name: string
-    isNew: boolean
-    field: ManagedCollectionField | null
-}
-
-export function listDataSourcesIds(): string[] {
+export function getDataSourcesIds(): string[] {
     return ["articles", "categories"]
 }
 
-export async function getDataSources(collection: string): Promise<DataSource> {
-    return await fetch(`/datasources/${collection}.json`).then(response => response.json())
+export async function getDataSource(dataSourceId: string): Promise<DataSource> {
+    return await fetch(`/datasources/${dataSourceId}.json`).then(response => response.json())
 }
 
-export function computeFieldConfigs(existingFields: ManagedCollectionField[], dataSource: DataSource) {
-    const result: FieldConfig[] = []
-    const fields = dataSource.fields
+export function computeFieldsFromDataSource(dataSource: DataSource): ManagedCollectionField[] {
+    const fields: ManagedCollectionField[] = []
 
-    for (const [name, fieldType] of Object.entries(fields)) {
-        const fieldId = generateHash(name)
-        let newField: ManagedCollectionField | null = null
+    for (const [fieldId, field] of Object.entries(dataSource.fields)) {
+        let type = field.type
 
-        const existingField = existingFields.find(field => field.id === fieldId)
-        if (existingField) {
-            newField = existingField
-        } else {
-            newField = {
-                id: fieldId,
-                name,
-                type: fieldType.type,
-                userEditable: false,
-            } as ManagedCollectionField
+        /**
+         * Here you can add support for other types, usually you would want to convert your APIs types to Framer types.
+         */
+
+        if (type === "html") {
+            type = "formattedText"
         }
 
-        result.push({
-            name,
-            isNew: !existingField,
-            field: newField,
-        })
+        fields.push({
+            id: fieldId,
+            name: fieldId,
+            type,
+            userEditable: false,
+        } as ManagedCollectionField)
     }
 
-    return result
+    return fields
+}
+
+export function mergeFieldsWithExistingFields(
+    originalFields: ManagedCollectionField[],
+    existingFields: ManagedCollectionField[]
+): ManagedCollectionField[] {
+    return originalFields.map(field => {
+        const existingField = existingFields.find(existingField => existingField.id === field.id)
+
+        return { ...field, name: existingField?.name ?? field.name }
+    })
 }
 
 export async function syncCollection(
-    collection: DataSource,
-    fieldConfigs: FieldConfig[],
+    collection: ManagedCollection,
+    dataSource: DataSource,
+    fields: ManagedCollectionField[],
     slugFieldId: string
 ): Promise<void> {
-    const activeCollection = await framer.getManagedCollection()
-    const unsyncedItems = new Set(await activeCollection.getItemIds())
-
     const items: CollectionItemData[] = []
+    const unsyncedItems = new Set(await collection.getItemIds())
 
-    const slugField = fieldConfigs.find(fieldConfig => fieldConfig.field?.id === slugFieldId)
+    const slugField = fields.find(field => field.id === slugFieldId)
     if (!slugField) {
         framer.notify("Slug field not found", {
             variant: "error",
@@ -76,57 +78,48 @@ export async function syncCollection(
         return
     }
 
-    for (const item of collection.items) {
-        const slugValue = item[slugField.name]
-        if (typeof slugValue !== "string") {
-            framer.notify(`Skipping item ${item.id} because it doesn't have a slug`, {
+    for (const item of dataSource.items) {
+        const slugValue = item[slugField.id]
+        if (typeof slugValue !== "string" || !slugValue) {
+            framer.notify(`Skipping item ${item.id} because it doesn't have a valid slug`, {
                 variant: "warning",
             })
             continue
         }
 
-        const itemId = generateHash(slugValue)
-        unsyncedItems.delete(itemId)
+        unsyncedItems.delete(slugValue)
 
         const fieldData: CollectionItemData["fieldData"] = {}
         for (const [fieldName, value] of Object.entries(item)) {
-            const fieldConfig = fieldConfigs.find(fieldConfig => fieldConfig.name === fieldName)
+            const field = fields.find(field => field.id === fieldName)
 
             // Field is in the data but should not be synced
-            if (!fieldConfig?.field) {
+            if (!field) {
                 console.warn(`Skipping field ${fieldName} because it may have been ignored`)
                 continue
             }
 
             // In a real-world scenario, we would need to convert the value to the correct type
-            fieldData[fieldConfig.field.id] = value
+            fieldData[field.id] = value
         }
 
         items.push({
-            id: itemId,
+            id: slugValue,
             slug: slugValue,
             draft: false,
             fieldData,
         })
     }
 
-    await activeCollection.setFields(fieldConfigs.map(fieldConfig => fieldConfig.field).filter(isNotNull))
-    await activeCollection.removeItems(Array.from(unsyncedItems))
-    await activeCollection.addItems(items)
+    await collection.setFields(fields)
+    await collection.removeItems(Array.from(unsyncedItems))
+    await collection.addItems(items)
 
-    await activeCollection.setPluginData(PLUGIN_KEYS.SYNC_REFERENCE, collection.id)
-    await activeCollection.setPluginData(PLUGIN_KEYS.SYNC_SLUG, slugFieldId)
+    await collection.setPluginData(PLUGIN_KEYS.COLLECTION_ID, dataSource.id)
+    await collection.setPluginData(PLUGIN_KEYS.SLUG_FIELD_ID, slugFieldId)
 }
 
-type SyncResult =
-    | { status: "success" }
-    | { status: "needsSetup"; allDataSources: string[] }
-    | {
-          status: "needsConfiguration"
-          dataSource: DataSource
-          existingFields: ManagedCollectionField[]
-          savedFieldsConfig: FieldConfig[]
-      }
+type SyncResult = "success" | "needsSetup" | "needsConfiguration"
 
 export async function syncExistingCollection(
     collection: ManagedCollection,
@@ -134,33 +127,24 @@ export async function syncExistingCollection(
     slugFieldId: string | null
 ): Promise<SyncResult> {
     if (!dataSourceId) {
-        return { status: "needsSetup", allDataSources: listDataSourcesIds() }
+        return "needsSetup"
     }
 
-    const syncDataSource = await getDataSources(dataSourceId)
-    if (!syncDataSource) {
-        return { status: "needsSetup", allDataSources: listDataSourcesIds() }
+    if (framer.mode !== "syncManagedCollection" || !slugFieldId) {
+        return "needsConfiguration"
     }
 
+    const dataSource = await getDataSource(dataSourceId)
     const existingFields = await collection.getFields()
-    const savedFieldsConfig = computeFieldConfigs(existingFields, syncDataSource)
-
-    if (!slugFieldId || framer.mode !== "syncManagedCollection") {
-        return { status: "needsConfiguration", dataSource: syncDataSource, existingFields, savedFieldsConfig }
-    }
 
     try {
-        await syncCollection(
-            syncDataSource,
-            savedFieldsConfig.filter(field => field.field && !field.isNew),
-            slugFieldId
-        )
-        return { status: "success" }
+        await syncCollection(collection, dataSource, existingFields, slugFieldId)
+        return "success"
     } catch (error) {
         console.error(error)
         framer.notify(`Failed to sync collection`, {
             variant: "error",
         })
-        return { status: "needsConfiguration", dataSource: syncDataSource, existingFields, savedFieldsConfig }
+        return "needsConfiguration"
     }
 }

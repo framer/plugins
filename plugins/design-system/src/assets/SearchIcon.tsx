6442956031cb61ba5c42859b5679512171dc18c0
Hunter Caron <huntercaron@me.com>
2025-02-28 15:07:16 +0100

    Initial pass

---
+++ plugins/design-system/src/assets/SearchIcon.tsx
@@ -0,0 +1,10 @@
+export function SearchIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+      <path
+        fill="currentColor"
+        d="M7.5 2a5.5 5.5 0 0 1 4.383 8.823l1.885 1.884a.75.75 0 1 1-1.061 1.061l-1.884-1.885A5.5 5.5 0 1 1 7.5 2Zm-4 5.5a4 4 0 1 0 8 0 4 4 0 0 0-8 0Z"
+      ></path>
+    </svg>
+  )
+}

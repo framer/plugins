f3292adcd2d49ca5076c3d9de7fd230026bd03a7
Alec Mev <alec@framer.com>
2025-02-28 15:43:31 +0000

    Tidy up ESLint

--- starters/cms/eslint.config.js
+++ starters/cms/eslint.config.js
@@ -7,19 +7,15 @@ import tseslint from "typescript-eslint"
 export default tseslint.config(
     { ignores: ["dist"] },
     {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ["**/*.{ts,tsx}"],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs["recommended-latest"],
+            reactRefresh.configs.vite,
+        ],
         languageOptions: {
-            ecmaVersion: 2022,
             globals: globals.browser,
-        },
-        plugins: {
-            "react-hooks": reactHooks,
-            "react-refresh": reactRefresh,
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
-            "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
         },
     }
 )

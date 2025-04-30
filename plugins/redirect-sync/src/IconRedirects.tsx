1e4d5beb39190911b1aa8272c165a4aec308d158
Tom Watson <tom@framer.com>
2025-04-30 17:00:28 +0200

    Fix bad jsx svg props

--- plugins/redirect-sync/src/IconRedirects.tsx
+++ plugins/redirect-sync/src/IconRedirects.tsx
@@ -4,35 +4,35 @@ export function IconRedirects() {
             <path
                 d="M 2 5 L 11 5"
                 fill="transparent"
-                stroke-width="2"
+                strokeWidth="2"
                 stroke="rgb(102, 102, 255)"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            ></path>
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
             <path
                 d="M 4.5 8.5 L 1 5 L 4.5 1.5"
                 fill="transparent"
-                stroke-width="2"
+                strokeWidth="2"
                 stroke="rgb(102, 102, 255)"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            ></path>
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
             <path
                 d="M 13.5 16.5 L 17 13 L 13.5 9.5"
                 fill="transparent"
-                stroke-width="2"
+                strokeWidth="2"
                 stroke="rgb(102, 102, 255)"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            ></path>
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
             <path
                 d="M 7 13 L 15 13"
                 fill="transparent"
-                stroke-width="2"
+                strokeWidth="2"
                 stroke="rgb(102, 102, 255)"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            ></path>
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
         </svg>
     )
 }

d1a2e558eee8c720297253e38137074037039b64
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-05-05 06:23:55 -0400

    More URL, email and phone number field types

--- plugins/notion/src/notion.ts
+++ plugins/notion/src/notion.ts
@@ -70,6 +70,8 @@ const pageCoverImageProperty: SupportedNotionProperty = {
 }
 
 const imageFileExtensions = ["jpg", "jpeg", "png", "gif", "apng", "webp", "svg"]
+const EMAIL_REGEX = /\S[^\s@]*@\S+\.\S+/
+const PHONE_REGEX = /^(\+?[0-9])[0-9]{7,14}$/
 
 const defaultValueForFieldType: Record<ManagedCollectionField["type"], unknown> = {
     string: "",
@@ -247,11 +249,11 @@ export const supportedCMSTypeByNotionPropertyType = {
     last_edited_time: ["date"],
     select: ["enum"],
     status: ["enum"],
-    url: ["link", "string"],
-    email: ["string", "formattedText"],
+    url: ["link", "image", "file", "string"],
+    email: ["string", "formattedText", "link"],
     files: ["file", "image"],
     relation: ["multiCollectionReference"],
-    phone_number: ["string"],
+    phone_number: ["string", "link"],
     unique_id: ["string", "number"],
     people: ["string"],
     created_by: ["string"],
@@ -278,7 +280,7 @@ function assertFieldTypeMatchesPropertyType<T extends SupportedPropertyType>(
  * That maps the Notion Property to the Framer CMS collection property type
  */
 export function getCollectionFieldForProperty<
-    TProperty extends Extract<NotionProperty, { type: SupportedPropertyType | CustomPropertyType }>
+    TProperty extends Extract<NotionProperty, { type: SupportedPropertyType | CustomPropertyType }>,
 >(
     property: TProperty,
     fieldType: ManagedCollectionField["type"],
@@ -500,6 +502,8 @@ export function getFieldDataEntryInput(
         case "email": {
             if (supportsHtml) {
                 return `<p>${property.email ?? ""}</p>`
+            } else if (fieldType === "link" && EMAIL_REGEX.test(property.email ?? "")) {
+                return `mailto:${property.email}`
             }
 
             return property.email ?? ""
@@ -537,9 +541,15 @@ export function getFieldDataEntryInput(
             }
         }
         case "url": {
-            return {
-                type: "link",
-                value: property.url,
+            const value = property.url ?? ""
+            switch (fieldType) {
+                case "link":
+                case "file":
+                    return isValidUrl(value) ? value : ""
+                case "image":
+                    return imageFileExtensions.some(ext => value.toLowerCase().endsWith(ext)) ? value : ""
+                default:
+                    return value
             }
         }
         case "unique_id": {
@@ -584,6 +594,10 @@ export function getFieldDataEntryInput(
             return ""
         }
         case "phone_number": {
+            if (fieldType === "link" && PHONE_REGEX.test(property.phone_number ?? "")) {
+                return `tel:${property.phone_number}`
+            }
+
             return property.phone_number ?? ""
         }
         case "people": {
@@ -634,7 +648,9 @@ export function getFieldDataEntryInput(
             switch (value?.type) {
                 case "array":
                     const item = value.array[0]
-                    result = item ? getPropertyValue(item, { fieldType }) : defaultValueForFieldType[fieldType] ?? null
+                    result = item
+                        ? getPropertyValue(item, { fieldType })
+                        : (defaultValueForFieldType[fieldType] ?? null)
                     break
                 case "number":
                     result = value.number ?? 0

8a2922b82f230ed877d579d2e6e4aaca85624501
Cédric Boirard <cedric@framer.com>
2025-05-06 12:42:07 +0200

    Update icons

--- plugins/redirect-sync/src/IconRedirects.tsx
+++ plugins/redirect-sync/src/IconRedirects.tsx
@@ -1,38 +1,8 @@
 export function IconRedirects() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none">
-            <path
-                d="M 2 5 L 11 5"
-                fill="transparent"
-                strokeWidth="2"
-                stroke="rgb(102, 102, 255)"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M 4.5 8.5 L 1 5 L 4.5 1.5"
-                fill="transparent"
-                strokeWidth="2"
-                stroke="rgb(102, 102, 255)"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M 13.5 16.5 L 17 13 L 13.5 9.5"
-                fill="transparent"
-                strokeWidth="2"
-                stroke="rgb(102, 102, 255)"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
-            <path
-                d="M 7 13 L 15 13"
-                fill="transparent"
-                strokeWidth="2"
-                stroke="rgb(102, 102, 255)"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none">
+            <path d="M 6 17.5 L 6 9 C 6 6.239 8.239 4 11 4 L 11 4 C 13.761 4 16 6.239 16 9 L 16 17.5" fill="transparent" strokeWidth="2" stroke="rgb(103, 102, 255)" strokeLinecap="round" strokeLinejoin="round"></path>
+            <path d="M 12.5 14.5 L 16 18 L 19.5 14.5" fill="transparent" strokeWidth="2" stroke="rgb(103, 102, 255)" strokeLinecap="round" strokeLinejoin="round"></path>
         </svg>
     )
 }

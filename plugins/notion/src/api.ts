45559fd293b499bdffbc5f0cd76f641fb5aa85bd
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-05-10 13:15:03 -0400

    Progress on Notion plugin

--- plugins/notion/src/api.ts
+++ plugins/notion/src/api.ts
@@ -55,6 +55,28 @@ export const pageContentProperty: SupportedNotionProperty = {
     rich_text: {},
 }
 
+// The order in which we display slug fields
+const preferedSlugFieldOrder: NotionProperty["type"][] = ["title", "rich_text"]
+
+export const supportedNotionPropertyTypes = [
+    "email",
+    "rich_text",
+    "date",
+    "last_edited_time",
+    "select",
+    "number",
+    "checkbox",
+    "created_time",
+    "title",
+    "status",
+    "url",
+    "files",
+    "relation",
+] satisfies ReadonlyArray<NotionProperty["type"]>
+
+type SupportedPropertyType = (typeof supportedNotionPropertyTypes)[number]
+type SupportedNotionProperty = Extract<NotionProperty, { type: SupportedPropertyType }>
+
 // Naive implementation to be authenticated, a token could be expired.
 // For simplicity we just close the plugin and clear storage in that case.
 export function isAuthenticated() {
@@ -97,25 +119,6 @@ export function initNotionClient() {
         auth: token,
     })
 }
-
-export const supportedNotionPropertyTypes = [
-    "email",
-    "rich_text",
-    "date",
-    "last_edited_time",
-    "select",
-    "number",
-    "checkbox",
-    "created_time",
-    "title",
-    "status",
-    "url",
-    "files",
-    "relation",
-] satisfies ReadonlyArray<NotionProperty["type"]>
-
-type SupportedPropertyType = (typeof supportedNotionPropertyTypes)[number]
-type SupportedNotionProperty = Extract<NotionProperty, { type: SupportedPropertyType }>
 
 export function isSupportedNotionProperty(property: NotionProperty): property is SupportedNotionProperty {
     return supportedNotionPropertyTypes.includes(property.type as SupportedPropertyType)
@@ -176,4 +179,129 @@ export async function getDatabase(databaseId: string) {
 
 export function richTextToPlainText(richText: RichTextItemResponse[] | undefined) {
     return Array.isArray(richText) ? richText.map(value => value.plain_text).join("") : ""
+}
+
+export function getNotionProperties(database: GetDatabaseResponse) {
+    const result: NotionProperty[] = []
+
+    // This property is always there but not included in `"database.properties"
+    result.push(pageContentProperty)
+
+    for (const key in database.properties) {
+        const property = database.properties[key]
+        assert(property)
+
+        result.push(property)
+    }
+
+    return result
+}
+
+/**
+ * Given a Notion Database returns a list of possible fields that can be used as
+ * a slug. And a suggested field id to use as a slug.
+ */
+export function getPossibleSlugFields(database: GetDatabaseResponse) {
+    const options: NotionProperty[] = []
+
+    for (const key in database.properties) {
+        const property = database.properties[key]
+        assert(property)
+
+        switch (property.type) {
+            case "title":
+            case "rich_text":
+                options.push(property)
+                break
+        }
+    }
+
+    function getOrderIndex(type: NotionProperty["type"]): number {
+        const index = preferedSlugFieldOrder.indexOf(type)
+        return index === -1 ? preferedSlugFieldOrder.length : index
+    }
+
+    options.sort((a, b) => getOrderIndex(a.type) - getOrderIndex(b.type))
+
+    return options
+}
+
+export function getPropertyValue(
+    property: PageObjectResponse["properties"][string],
+    { supportsHtml }: { supportsHtml: boolean }
+): unknown | undefined {
+    switch (property.type) {
+        case "checkbox": {
+            return property.checkbox
+        }
+        case "last_edited_time": {
+            return property.last_edited_time
+        }
+        case "created_time": {
+            return property.created_time
+        }
+        case "rich_text": {
+            if (supportsHtml) {
+                return richTextToHTML(property.rich_text)
+            }
+
+            return richTextToPlainText(property.rich_text)
+        }
+        case "select": {
+            if (!property.select) return null
+
+            return property.select.id
+        }
+        case "title":
+            if (supportsHtml) {
+                return richTextToHTML(property.title)
+            }
+
+            return richTextToPlainText(property.title)
+        case "number": {
+            return property.number
+        }
+        case "url": {
+            return property.url
+        }
+        case "unique_id": {
+            return property.unique_id.number
+        }
+        case "date": {
+            return property.date?.start
+        }
+        case "relation": {
+            return property.relation.map(({ id }) => id)
+        }
+        case "files": {
+            const firstFile = property.files[0]
+            if (!firstFile) return null
+
+            if (firstFile.type === "external") {
+                return firstFile.external.url
+            }
+
+            if (firstFile.type === "file") {
+                return firstFile.file.url
+            }
+        }
+    }
+}
+
+async function getPageBlocksAsRichText(pageId: string) {
+    assert(notion, "Notion client is not initialized")
+
+    const blocksIterator = iteratePaginatedAPI(notion.blocks.children.list, {
+        block_id: pageId,
+    })
+
+    const blocks: BlockObjectResponse[] = []
+    for await (const block of blocksIterator) {
+        if (!isFullBlock(block)) continue
+        blocks.push(block)
+    }
+
+    assert(blocks.every(isFullBlock), "Response is not a full block")
+
+    return blocksToHtml(blocks)
 }

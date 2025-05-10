ee26a1ea9c9a31014414fa6f525023858bee15cd
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-05-09 22:30:00 -0400

    Update select data source page

--- plugins/notion/src/SelectDataSource.tsx
+++ plugins/notion/src/SelectDataSource.tsx
@@ -31,18 +31,11 @@ export function SelectDataSource({ onSelectDataSource }: SelectDataSourceProps) 
     return (
         <main className="framer-hide-scrollbar setup">
             <div className="intro">
-                <div className="logo">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none">
-                        <path
-                            fill="currentColor"
-                            d="M15.5 8c3.59 0 6.5 1.38 6.5 3.083 0 1.702-2.91 3.082-6.5 3.082S9 12.785 9 11.083C9 9.38 11.91 8 15.5 8Zm6.5 7.398c0 1.703-2.91 3.083-6.5 3.083S9 17.101 9 15.398v-2.466c0 1.703 2.91 3.083 6.5 3.083s6.5-1.38 6.5-3.083Zm0 4.316c0 1.703-2.91 3.083-6.5 3.083S9 21.417 9 19.714v-2.466c0 1.702 2.91 3.083 6.5 3.083S22 18.95 22 17.248Z"
-                        />
-                    </svg>
-                </div>
-                <div className="content">
-                    <h2>CMS Starter</h2>
-                    <p>Everything you need to get started with a CMS Plugin.</p>
-                </div>
+                <img src="/notion-connect.png" className="select-database-img" />
+                <p>
+                    To manually connect a database, open it in Notion, click on the three dots icon in the top right
+                    corner, then select Connections and connect to Framer.
+                </p>
             </div>
 
             <form onSubmit={handleSubmit}>
@@ -53,7 +46,7 @@ export function SelectDataSource({ onSelectDataSource }: SelectDataSourceProps) 
                         value={selectedDataSourceId}
                     >
                         <option value="" disabled>
-                            Choose Source…
+                            Choose Database
                         </option>
                         {dataSourceOptions.map(({ id, name }) => (
                             <option key={id} value={id}>

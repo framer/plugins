f0920b188101e48d0d7919094974452e18af82a5
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-05-09 22:19:42 -0400

    Create CMS starter for new Notion plugin

---
+++ plugins/notion/src/App.tsx
@@ -0,0 +1,76 @@
+import "./App.css"
+
+import { framer, type ManagedCollection } from "framer-plugin"
+import { useEffect, useLayoutEffect, useState } from "react"
+import { type DataSource, getDataSource } from "./data"
+import { FieldMapping } from "./FieldMapping"
+import { SelectDataSource } from "./SelectDataSource"
+
+interface AppProps {
+    collection: ManagedCollection
+    previousDataSourceId: string | null
+    previousSlugFieldId: string | null
+}
+
+export function App({ collection, previousDataSourceId, previousSlugFieldId }: AppProps) {
+    const [dataSource, setDataSource] = useState<DataSource | null>(null)
+    const [isLoadingDataSource, setIsLoadingDataSource] = useState(Boolean(previousDataSourceId))
+
+    useLayoutEffect(() => {
+        const hasDataSourceSelected = Boolean(dataSource)
+
+        framer.showUI({
+            width: hasDataSourceSelected ? 360 : 260,
+            height: hasDataSourceSelected ? 425 : 340,
+            minWidth: hasDataSourceSelected ? 360 : undefined,
+            minHeight: hasDataSourceSelected ? 425 : undefined,
+            resizable: hasDataSourceSelected,
+        })
+    }, [dataSource])
+
+    useEffect(() => {
+        if (!previousDataSourceId) {
+            return
+        }
+
+        const abortController = new AbortController()
+
+        setIsLoadingDataSource(true)
+        getDataSource(previousDataSourceId, abortController.signal)
+            .then(setDataSource)
+            .catch(error => {
+                if (abortController.signal.aborted) return
+
+                console.error(error)
+                framer.notify(
+                    `Error loading previously configured data source “${previousDataSourceId}”. Check the logs for more details.`,
+                    {
+                        variant: "error",
+                    }
+                )
+            })
+            .finally(() => {
+                if (abortController.signal.aborted) return
+
+                setIsLoadingDataSource(false)
+            })
+
+        return () => {
+            abortController.abort()
+        }
+    }, [previousDataSourceId])
+
+    if (isLoadingDataSource) {
+        return (
+            <main className="loading">
+                <div className="framer-spinner" />
+            </main>
+        )
+    }
+
+    if (!dataSource) {
+        return <SelectDataSource onSelectDataSource={setDataSource} />
+    }
+
+    return <FieldMapping collection={collection} dataSource={dataSource} initialSlugFieldId={previousSlugFieldId} />
+}

b9d6b31a9946a854db1c1d4a02d592b75ff5c7a8
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-06-18 14:14:37 -0400

    Remove legacy Notion plugin

--- notion-legacy/src/components/CenteredSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "./Spinner"
-
-export function CenteredSpinner() {
-    return (
-        <div className="w-full h-full flex items-center justify-center">
-            <Spinner size="medium" />
-        </div>
-    )
-}

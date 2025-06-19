03e4a5d7369c734284900cfe42180c8620e0d84f
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-06-19 07:56:52 -0400

    Remove color field type

--- plugins/airtable/src/data.ts
+++ plugins/airtable/src/data.ts
@@ -299,6 +299,7 @@ export async function getItems(dataSource: DataSource, slugFieldId: string) {
                     case "image":
                     case "file":
                     case "link":
+                    case "color":
                     case "date":
                     case "color":
                     case "collectionReference":
@@ -306,12 +307,6 @@ export async function getItems(dataSource: DataSource, slugFieldId: string) {
                         fieldData[field.id] = {
                             value: null,
                             type: field.type,
-                        }
-                        break
-                    case "color":
-                        fieldData[field.id] = {
-                            value: "#000000",
-                            type: "color",
                         }
                         break
                     default:

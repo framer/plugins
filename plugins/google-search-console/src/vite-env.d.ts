dcefb7abd63d6523517b1e7e51b6632d64dc4273
Alec Mev <alec@framer.com>
2025-06-19 22:03:15 +0100

    Make vite-env.d.ts strict
    Re-add to CMS Starter, it's useful enough, even if unused.

--- plugins/google-search-console/src/vite-env.d.ts
+++ plugins/google-search-console/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_MOCK_DATA?: string;
+  readonly VITE_OAUTH_API_DOMAIN?: string;
+}
--- plugins/renamer/src/vite-env.d.ts
+++ plugins/renamer/src/vite-env.d.ts
@@ -1,2 +1,6 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}

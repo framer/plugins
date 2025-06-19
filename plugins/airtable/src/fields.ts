205d92f89188beb22e04722f90eabf2b7dea9b03
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-06-19 07:56:52 -0400

    Remove color field type

--- plugins/airtable/src/fields.ts
+++ plugins/airtable/src/fields.ts
@@ -125,7 +125,7 @@ function inferStringField(fieldSchema: AirtableFieldSchema & { type: "singleLine
         airtableType: fieldSchema.type,
         airtableOptions: fieldSchema.options,
         type: "string",
-        allowedTypes: ["string", "formattedText", "color"],
+        allowedTypes: ["string", "formattedText"],
     }
 }
 

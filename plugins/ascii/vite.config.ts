a5ab8d3baefe5b7dec63af87108263bbe06a3476
Alec Mev <alec@framer.com>
2025-06-19 22:12:29 +0100

    Use vite-config via global Vite scripts
    Renamer remains untouched because it needs the Svelte plugin, will be
    fixed when converted to React. CMS Starter untouched as it's supposed
    to be copy-friendly, might be fixed if the config is ever published.

--- plugins/ascii/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-import framer from "vite-plugin-framer"
-import mkcert from "vite-plugin-mkcert"
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), mkcert(), framer()],
-})

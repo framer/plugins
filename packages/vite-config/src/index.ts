2e9ce7212d1f385fcb09a001df0746c62e4fd5f9
Alec Mev <alec@framer.com>
2025-06-25 18:34:24 +0100

    Major-update Tailwind in Sheets
    Add @tailwindcss/vite.

--- packages/vite-config/src/index.ts
+++ packages/vite-config/src/index.ts
@@ -1,6 +1,7 @@
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
 import type { UserConfig } from "vite"
 import framer from "vite-plugin-framer"
 import mkcert from "vite-plugin-mkcert"
 
-export default { plugins: [react(), mkcert(), framer()] } satisfies UserConfig
+export default { plugins: [react(), mkcert(), framer(), tailwindcss()] } satisfies UserConfig
--- plugins/google-sheets/src/components/CheckboxTextField.tsx
+++ plugins/google-sheets/src/components/CheckboxTextField.tsx
@@ -30,7 +30,7 @@ export function CheckboxTextfield({ value, darken, checked, onChange, disabled }
                 checked={checked}
                 onChange={handleCheckboxChange}
                 onClick={e => e.stopPropagation()}
-                className="checked:!bg-sheets-green focus:ring-1 focus:ring-sheets-green checked:border-none dark:!bg-[#777] !bg-[#CCC]"
+                className="checked:bg-sheets-green! focus:ring-1 focus:ring-sheets-green checked:border-none dark:bg-[#777]! bg-[#CCC]!"
                 disabled={disabled}
             />
             <input

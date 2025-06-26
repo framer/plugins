e2b5e097ded38504f7c77bdcf6f7182f553e1bc4
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-06-26 15:21:40 -0400

    Update Notion config

--- plugins/notion/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-import framer from "vite-plugin-framer"
-import mkcert from "vite-plugin-mkcert"
-
-export default defineConfig({ plugins: [react(), mkcert(), framer()] })

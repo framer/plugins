35b571cbadcd20be30578804f7862ba53b637f06
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-06-30 11:55:21 -0400

    Remove isWorkerAlive check

--- plugins/notion/src/auth.ts
+++ plugins/notion/src/auth.ts
@@ -1,5 +1,5 @@
+import { API_BASE_URL, PLUGIN_KEYS } from "./api"
 import { generateRandomId } from "./utils"
-import { PLUGIN_KEYS, API_BASE_URL } from "./api"
 
 type Tokens = {
     bearer_token: string
@@ -63,20 +63,6 @@ class Auth {
             readKey,
             url: `https://api.notion.com/v1/oauth/authorize?client_id=${this.NOTION_CLIENT_ID}&response_type=code&owner=user&redirect_uri=${oauthRedirectUrl}&state=${writeKey}`,
         }
-    }
-
-    async isWorkerAlive(): Promise<boolean> {
-        return true
-        // try {
-        //     const res = await fetch(`${API_BASE_URL}/auth/authorize`, {
-        //         method: "POST",
-        //     })
-        //     console.log("res", res)
-        //     return res.status === 400
-        // } catch (error) {
-        //     console.error("Failed to connect to OAuth worker:", error)
-        //     return false
-        // }
     }
 
     private readonly tokens = {

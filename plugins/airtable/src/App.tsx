2545ca3135b1c4da595e723aef1ee466b37354ed
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-06-30 08:58:01 -0400

    Revert window resizing

--- plugins/airtable/src/App.tsx
+++ plugins/airtable/src/App.tsx
@@ -23,7 +23,7 @@ export function App({ collection, previousBaseId, previousTableId, previousSlugF
     const [noTableAccess, setNoTableAccess] = useState(false)
 
     useLayoutEffect(() => {
-        const hasDataSourceSelected = Boolean(dataSource) || isLoadingDataSource
+        const hasDataSourceSelected = Boolean(dataSource)
 
         framer.showUI({
             width: hasDataSourceSelected ? 425 : 320,

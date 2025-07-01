a83a34d52f3ee86446ebbde2257181dac77c2317
Clément Roche <rchclement@gmail.com>
2025-07-01 16:23:50 +0200

    review n1

--- plugins/ashby/src/App.tsx
+++ plugins/ashby/src/App.tsx
@@ -25,7 +25,7 @@ export function App({ collection, previousDataSourceId, previousSlugFieldId, pre
 
         framer.showUI({
             width: hasDataSourceSelected ? 400 : 320,
-            height: hasDataSourceSelected ? 427 : 325,
+            height: hasDataSourceSelected ? 427 : 345,
             minHeight: hasDataSourceSelected ? 427 : undefined,
             minWidth: hasDataSourceSelected ? 400 : undefined,
             resizable: hasDataSourceSelected,

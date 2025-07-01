c3908e9f741aeb377f60786af456d441d3479819
Clément Roche <rchclement@gmail.com>
2025-07-01 13:11:32 +0200

    offers fields

--- plugins/recruitee/src/api-types.ts
+++ plugins/recruitee/src/api-types.ts
@@ -2,28 +2,34 @@ export interface Offer {
     id: number
     title: string
     department: string | null
+    department_id: number | null
     employment_type: string
     status: string
     candidates_count: number
+    qualified_candidates_count: number
     hired_candidates_count: number
+    hired_candidates_without_openings_count: number | null
     disqualified_candidates_count: number
     location: string | null
+    location_ids: number[]
     street: string | null
     city: string | null
     state_name: string | null
     postal_code: string | null
     country_code: string | null
-    mailbox_email: string
-    requirements: string
-    description: string
-    offer_tags: string
-    url: string
+    mailbox_email: string | null
+    requirements: string | null
+    description: string | null
+    offer_tags: string[] | null
+    tags: string[]
+    url: string | null
+    slug: string
     adminapp_url: string | null
     careers_url: string | null
     state_code: string | null
     kind: string | null
     shared_openings_count: number | null
-    dynamic_fields_count: string | null
+    dynamic_fields_count: number | null
     has_active_campaign: boolean
     enabled_for_referrals: boolean
     number_of_openings: number | null
@@ -31,15 +37,27 @@ export interface Offer {
     job_scheduler: string | null
     position: number | null
     lang_code: string | null
-    eeo_settings: string | null
+    enabled_languages: string[]
+    // eeo_settings: string | null // we dont know what this is (it's not documented and we can't activate it)
     hiring_manager_id: number | null
+    recruiter_id: number | null
     pipeline_template_id: number | null
+    // pipeline_template: Record<string, unknown> | null
+    pipeline: boolean
     priority: string | null
     on_site: boolean
     hybrid: boolean
     remote: boolean
     published_at: string | null
     closed_at: string | null
+    created_at: string | null
+    updated_at: string | null
+    guid: string
+    example: boolean
+    followed: boolean
+    // followers: Record<string, unknown> | null
+    // issues: Record<string, unknown> | null
+    wysiwyg_editor: string | null
 }
 export interface Location {
     id: number
@@ -70,7 +88,7 @@ export interface Candidate {
     id: number
     initials: string | null
     name: string
-    example: string | null
+    example: boolean
     emails: string
     phones: string
     photo_thumb_url: string | null
@@ -117,113 +135,155 @@ export function validateOffers(data: unknown): asserts data is Offer[] {
         if (typeof item.title !== "string") {
             throw new Error("Expected offer to have string title")
         }
-        if (item.department != null && typeof item.department !== "string") {
-            throw new Error("Expected candidates to have string department")
+        if (item.department !== null && typeof item.department !== "string") {
+            throw new Error("Expected offer to have string department")
         }
-        if (typeof item.employment_type !== "string") {
+        if (item.employment_type !== null && typeof item.employment_type !== "string") {
             throw new Error("Expected offer to have string employment type")
         }
-        if (typeof item.status !== "string") {
+        if (item.status !== null && typeof item.status !== "string") {
             throw new Error("Expected offer to have string status")
         }
-        if (typeof item.candidates_count !== "number") {
+        if (item.candidates_count !== null && typeof item.candidates_count !== "number") {
             throw new Error("Expected offer to have number candidates count")
         }
-        if (typeof item.hired_candidates_count !== "number") {
+        if (item.hired_candidates_count !== null && typeof item.hired_candidates_count !== "number") {
             throw new Error("Expected offer to have number hired candidates count")
         }
-        if (typeof item.disqualified_candidates_count !== "number") {
+        if (item.disqualified_candidates_count !== null && typeof item.disqualified_candidates_count !== "number") {
             throw new Error("Expected offer to have number disqualified candidates count")
         }
-        if (typeof item.city !== "string") {
+        if (item.city !== null && typeof item.city !== "string") {
             throw new Error("Expected offer to have string city")
         }
-        if (typeof item.mailbox_email !== "string") {
+        if (item.mailbox_email !== null && typeof item.mailbox_email !== "string") {
             throw new Error("Expected offer to have string email")
         }
-        if (typeof item.requirements !== "string") {
+        if (item.requirements !== null && typeof item.requirements !== "string") {
             throw new Error("Expected offer to have string requirements")
         }
-        if (typeof item.description !== "string") {
+        if (item.description !== null && typeof item.description !== "string") {
             throw new Error("Expected offer to have string description")
         }
-        if (!Array.isArray(item.offer_tags)) {
-            throw new Error("Expected candidates to have string emails")
+        if (item.offer_tags !== null && !Array.isArray(item.offer_tags)) {
+            throw new Error("Expected offer to have array offer_tags")
         }
-        if (item.location != null && typeof item.location !== "string") {
-            throw new Error("Expected candidates to have string location")
+        if (item.location !== null && typeof item.location !== "string") {
+            throw new Error("Expected offer to have string location")
         }
-        if (item.street != null && typeof item.street !== "string") {
-            throw new Error("Expected candidates to have string street")
+        if (item.street !== null && typeof item.street !== "string") {
+            throw new Error("Expected offer to have string street")
         }
-        if (item.state_name != null && typeof item.state_name !== "string") {
-            throw new Error("Expected candidates to have string state name")
+        if (item.state_name !== null && typeof item.state_name !== "string") {
+            throw new Error("Expected offer to have string state name")
         }
-        if (item.postal_code != null && typeof item.postal_code !== "string") {
-            throw new Error("Expected candidates to have string postal code")
+        if (item.postal_code !== null && typeof item.postal_code !== "string") {
+            throw new Error("Expected offer to have string postal code")
         }
-        if (item.country_code != null && typeof item.country_code !== "string") {
-            throw new Error("Expected candidates to have string country code")
+        if (item.country_code !== null && typeof item.country_code !== "string") {
+            throw new Error("Expected offer to have string country code")
         }
-        if (item.enabled_for_referrals != null && typeof item.enabled_for_referrals !== "boolean") {
-            throw new Error("Expected candidates to have boolean enabled for referrals")
+        if (typeof item.enabled_for_referrals !== "boolean") {
+            throw new Error("Expected offer to have boolean enabled for referrals")
         }
-        if (item.on_site != null && typeof item.on_site !== "boolean") {
-            throw new Error("Expected candidates to have boolean on site")
+        if (typeof item.on_site !== "boolean") {
+            throw new Error("Expected offer to have boolean on site")
         }
-        if (item.remote != null && typeof item.remote !== "boolean") {
-            throw new Error("Expected candidates to have boolean remote")
+        if (typeof item.remote !== "boolean") {
+            throw new Error("Expected offer to have boolean remote")
         }
-        if (item.adminapp_url != null && typeof item.adminapp_url !== "string") {
-            throw new Error("Expected candidates to have string adminapp url")
+        if (item.adminapp_url !== null && typeof item.adminapp_url !== "string") {
+            throw new Error("Expected offer to have string adminapp url")
         }
-        if (item.careers_url != null && typeof item.careers_url !== "string") {
-            throw new Error("Expected candidates to have string careers url")
+        if (item.careers_url !== null && typeof item.careers_url !== "string") {
+            throw new Error("Expected offer to have string careers url")
         }
-        if (item.state_code != null && typeof item.state_code !== "string") {
-            throw new Error("Expected candidates to have string state code")
+        if (item.state_code !== null && typeof item.state_code !== "string") {
+            throw new Error("Expected offer to have string state code")
         }
-        if (item.kind != null && typeof item.kind !== "string") {
-            throw new Error("Expected candidates to have string kind")
+        if (item.kind !== null && typeof item.kind !== "string") {
+            throw new Error("Expected offer to have string kind")
         }
-        if (item.shared_openings_count != null && typeof item.shared_openings_count !== "number") {
-            throw new Error("Expected candidates to have number shared openings count")
+        if (item.shared_openings_count !== null && typeof item.shared_openings_count !== "number") {
+            throw new Error("Expected offer to have number shared openings count")
         }
-        if (item.dynamic_fields_count != null && typeof item.dynamic_fields_count !== "number") {
-            throw new Error("Expected candidates to have number dynamic fields count")
+        if (item.dynamic_fields_count !== null && typeof item.dynamic_fields_count !== "number") {
+            throw new Error("Expected offer to have number dynamic fields count")
         }
-        if (item.has_active_campaign != null && typeof item.has_active_campaign !== "boolean") {
-            throw new Error("Expected candidates to have boolean has active campaign")
+        if (typeof item.has_active_campaign !== "boolean") {
+            throw new Error("Expected offer to have boolean has active campaign")
         }
-        if (item.enabled_for_referrals != null && typeof item.enabled_for_referrals !== "boolean") {
-            throw new Error("Expected candidates to have boolean enabled for referrals")
+        if (item.number_of_openings !== null && typeof item.number_of_openings !== "number") {
+            throw new Error("Expected offer to have number number_of_openings")
         }
-        if (item.number_of_openings != null && typeof item.number_of_openings !== "number") {
-            throw new Error("Expected candidates to have number number_of_openings")
+        if (item.highlight_html !== null && typeof item.highlight_html !== "string") {
+            throw new Error("Expected offer to have string highlight_html")
         }
-        if (item.highlight_html != null && typeof item.highlight_html !== "string") {
-            throw new Error("Expected candidates to have string highlight_html")
+        if (item.job_scheduler !== null && typeof item.job_scheduler !== "string") {
+            throw new Error("Expected offer to have string job_scheduler")
         }
-        if (item.job_scheduler != null && typeof item.job_scheduler !== "string") {
-            throw new Error("Expected candidates to have string job_scheduler")
+        if (item.position !== null && typeof item.position !== "number") {
+            throw new Error("Expected offer to have number position")
         }
-        if (item.position != null && typeof item.position !== "number") {
-            throw new Error("Expected candidates to have number position")
+        if (item.lang_code !== null && typeof item.lang_code !== "string") {
+            throw new Error("Expected offer to have string lang_code")
         }
-        if (item.lang_code != null && typeof item.lang_code !== "string") {
-            throw new Error("Expected candidates to have string lang_code")
+        if (item.hiring_manager_id !== null && typeof item.hiring_manager_id !== "number") {
+            throw new Error("Expected offer to have number hiring_manager_id")
         }
-        if (item.eeo_settings != null && typeof item.eeo_settings !== "string") {
-            throw new Error("Expected candidates to have string eeo_settings")
+        if (item.pipeline_template_id !== null && typeof item.pipeline_template_id !== "number") {
+            throw new Error("Expected offer to have number pipeline_template_id")
         }
-        if (item.hiring_manager_id != null && typeof item.hiring_manager_id !== "number") {
-            throw new Error("Expected candidates to have number hiring_manager_id")
+        if (item.priority !== null && typeof item.priority !== "string") {
+            throw new Error("Expected offer to have string priority")
         }
-        if (item.pipeline_template_id != null && typeof item.pipeline_template_id !== "number") {
-            throw new Error("Expected candidates to have number pipeline_template_id")
+        if (item.department_id !== null && typeof item.department_id !== "number") {
+            throw new Error("Expected offer to have number department_id")
         }
-        if (item.priority != null && typeof item.priority !== "string") {
-            throw new Error("Expected candidates to have string priority")
+        if (item.location_ids !== null && !Array.isArray(item.location_ids)) {
+            throw new Error("Expected offer to have array location_ids")
+        }
+        if (item.tags !== null && !Array.isArray(item.tags)) {
+            throw new Error("Expected offer to have array tags")
+        }
+        if (typeof item.slug !== "string") {
+            throw new Error("Expected offer to have string slug")
+        }
+        if (item.enabled_languages !== null && !Array.isArray(item.enabled_languages)) {
+            throw new Error("Expected offer to have array enabled_languages")
+        }
+        if (item.recruiter_id !== null && typeof item.recruiter_id !== "number") {
+            throw new Error("Expected offer to have number recruiter_id")
+        }
+        if (item.qualified_candidates_count !== null && typeof item.qualified_candidates_count !== "number") {
+            throw new Error("Expected offer to have number qualified_candidates_count")
+        }
+        if (
+            item.hired_candidates_without_openings_count !== null &&
+            typeof item.hired_candidates_without_openings_count !== "number"
+        ) {
+            throw new Error("Expected offer to have number hired_candidates_without_openings_count")
+        }
+        if (typeof item.hybrid !== "boolean") {
+            throw new Error("Expected offer to have boolean hybrid")
+        }
+        if (item.created_at !== null && typeof item.created_at !== "string") {
+            throw new Error("Expected offer to have string created_at")
+        }
+        if (item.updated_at !== null && typeof item.updated_at !== "string") {
+            throw new Error("Expected offer to have string updated_at")
+        }
+        if (typeof item.guid !== "string") {
+            throw new Error("Expected offer to have string guid")
+        }
+        if (typeof item.example !== "boolean") {
+            throw new Error("Expected offer to have boolean example")
+        }
+        if (typeof item.followed !== "boolean") {
+            throw new Error("Expected offer to have boolean followed")
+        }
+        if (item.wysiwyg_editor !== null && typeof item.wysiwyg_editor !== "string") {
+            throw new Error("Expected offer to have string wysiwyg_editor")
         }
     }
 }

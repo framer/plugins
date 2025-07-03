ee0c3bae674873154735d6e761bcfdef3c12edcf
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-07-03 17:17:26 -0400

    Unmount React root

--- plugins/notion/src/main.tsx
+++ plugins/notion/src/main.tsx
@@ -18,13 +18,17 @@ const root = document.getElementById("root")
 if (!root) throw new Error("Root element not found")
 
 if (!tokens) {
+    const reactRoot = ReactDOM.createRoot(root)
+
     await new Promise<void>(resolve => {
-        ReactDOM.createRoot(root).render(
+        reactRoot.render(
             <React.StrictMode>
                 <Authenticate onAuthenticated={resolve} />
             </React.StrictMode>
         )
     })
+
+    reactRoot.unmount()
 }
 
 const [

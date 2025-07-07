d1d03766f961cc53534306f239ad5485bf832a82
Elmar Burke <elmar@framer.com>
2025-07-07 09:54:06 +0200

    Simplify FormatFromNow

--- plugins/code-versions/src/components/FormatFromNow.tsx
+++ plugins/code-versions/src/components/FormatFromNow.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { formatRelative } from "../utils/date"
 
 interface FormatFromNowProps {
@@ -6,25 +6,15 @@ interface FormatFromNowProps {
     locales?: Intl.LocalesArgument
 }
 
-function getInitialIntervalMs(diff: number) {
-    if (diff < 60 * 1000) return 10 * 1000 // update every ~10 seconds (since display shows "now" until 60s)
-    if (diff < 60 * 60 * 1000) return 60 * 1000 // update every minute
-    if (diff < 24 * 60 * 60 * 1000) return 60 * 60 * 1000 // update every hour
-    if (diff < 7 * 24 * 60 * 60 * 1000) return 24 * 60 * 60 * 1000 // update every day
-    return null // no update needed
-}
-
 export function FormatFromNow({ date, locales }: FormatFromNowProps) {
     const [now, setNow] = useState(() => new Date())
-    const targetDate = new Date(date)
-    const diff = now.getTime() - targetDate.getTime()
-    const [intervalMs] = useState(() => getInitialIntervalMs(diff))
+    const targetDate = useMemo(() => (typeof date === "string" ? new Date(date) : date), [date])
 
     useEffect(() => {
-        if (intervalMs === null) return
-        const id = setInterval(() => setNow(new Date()), intervalMs)
+        // shortest interval we show is Now and 1m, so 10s is enough
+        const id = setInterval(() => setNow(new Date()), 10_000)
         return () => clearInterval(id)
-    }, [intervalMs])
+    }, [])
 
     return <span>{formatRelative(now, targetDate, locales)}</span>
 }

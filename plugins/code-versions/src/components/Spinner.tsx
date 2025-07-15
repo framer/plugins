7ac91a99aa8cb8b336d30061834acaa3237bb29b
Elmar Burke <elmar@framer.com>
2025-07-15 10:04:59 +0200

    fixup! Introducing Spinner with a delay

--- plugins/code-versions/src/components/Spinner.tsx
+++ plugins/code-versions/src/components/Spinner.tsx
@@ -11,6 +11,8 @@ export function Spinner({ className }: SpinnerProps) {
                 "flex items-center justify-center z-10 animate-(--fade-in-animation) [animation-delay:800ms] opacity-0",
                 className
             )}
+            role="status"
+            aria-live="polite"
         >
             <div className="framer-spinner" />
         </div>

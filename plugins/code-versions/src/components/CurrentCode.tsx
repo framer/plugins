7b1c63c567de85ad572b05f4565d371ac87ecf73
Elmar Burke <elmar@framer.com>
2025-08-01 14:33:22 +0200

    refactor(code-versions): abstract code highlighting into resuable component

--- plugins/code-versions/src/components/CurrentCode.tsx
+++ plugins/code-versions/src/components/CurrentCode.tsx
@@ -1,10 +1,4 @@
-import { highlightElement } from "prismjs"
-import { useEffect, useRef } from "react"
-import "prismjs/components/prism-typescript"
-import "prismjs/components/prism-jsx"
-import "prismjs/components/prism-tsx"
-import "prismjs/plugins/line-numbers/prism-line-numbers"
-import "./CurrentCode.css"
+import { HighlightedCode } from "./HighlightedCode"
 
 interface CurrentCodeProps {
     code: string
@@ -15,21 +9,5 @@ interface CurrentCodeProps {
  * It shows all lines of the current version, and the one row of line numbers are shown on the left
  */
 export function CurrentCode({ code }: CurrentCodeProps) {
-    const codeRef = useRef<HTMLElement>(null)
-
-    useEffect(() => {
-        if (!codeRef.current) return
-
-        highlightElement(codeRef.current)
-    }, [code])
-
-    return (
-        <div className="current-code line-numbers animate-(--fade-in-animation)">
-            <pre className="font-mono text-code">
-                <code ref={codeRef} className="language-tsx">
-                    {code}
-                </code>
-            </pre>
-        </div>
-    )
+    return <HighlightedCode code={code} showLineNumbers className="animate-(--fade-in-animation)" />
 }

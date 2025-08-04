2ebb1326afe5366d2340315efb0e7bbb36cc49fc
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-08-04 13:22:52 -0400

    Revert tab style

--- plugins/renamer/src/components/Tabs.tsx
+++ plugins/renamer/src/components/Tabs.tsx
@@ -11,29 +11,13 @@ interface Props {
 }
 
 export default function Tabs({ items }: Props) {
-    const activeIndex = items.findIndex(item => item.active)
-
     return (
         <div className="tabs">
-            <div className="tabs-container">
-                <div className="tab-bg-container">
-                    {activeIndex !== -1 && (
-                        <div
-                            className="tab-bg"
-                            style={{
-                                width: `${100 / items.length}%`,
-                                left: `${(100 / items.length) * activeIndex}%`,
-                            }}
-                        />
-                    )}
-                </div>
-
-                {items.map((item, index) => (
-                    <button key={index} className={cx("tab", item.active && "active")} onClick={item.select}>
-                        {item.label}
-                    </button>
-                ))}
-            </div>
+            {items.map((item, index) => (
+                <button key={index} className={cx("tab", item.active && "active")} onClick={item.select}>
+                    {item.label}
+                </button>
+            ))}
         </div>
     )
 }

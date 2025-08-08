687ce87a435695acd61fc37c455115dfe6f3f382
Elmar Burke <hi@elmar.codes>
2025-08-08 10:33:04 +0200

    fixup! feat(global-search): adding indexer

--- plugins/global-search/src/utils/indexer/event-emitter.ts
+++ plugins/global-search/src/utils/indexer/event-emitter.ts
@@ -1,32 +1,28 @@
-import type { IndexEntry } from "./types"
-
-export interface Events {
-    upsert: { entry: IndexEntry }
-    error: { error: Error }
-    progress: { processed: number; total?: number }
-    started: void
-    completed: void
-    restarted: void
-    aborted: void
+export interface EventMap {
+    [eventName: string]: unknown
 }
-
-export class TypedEventEmitter {
+export class TypedEventEmitter<TypedEventMap extends EventMap> {
     private target = new EventTarget()
 
-    on<EventName extends keyof Events>(type: EventName, listener: (event: Events[EventName]) => void): () => void {
-        const eventListener = (e: Event) => {
-            const customEvent = e as CustomEvent<Events[EventName]>
-            listener(customEvent.detail)
+    on<EventName extends Extract<keyof TypedEventMap, string>>(
+        type: EventName,
+        listener: TypedEventMap[EventName] extends void ? () => void : (payload: TypedEventMap[EventName]) => void,
+        options?: AddEventListenerOptions | boolean
+    ): () => void {
+        const handler: EventListener = event => {
+            const { detail } = event as CustomEvent<TypedEventMap[EventName]>
+            listener(detail)
         }
 
-        this.target.addEventListener(type, eventListener)
-
-        return () => {
-            this.target.removeEventListener(type, eventListener)
-        }
+        this.target.addEventListener(type, handler, options)
+        return () => this.target.removeEventListener(type, handler, options)
     }
 
-    emit<EventName extends keyof Events>(type: EventName, detail: Events[EventName]) {
+    emit<EventName extends Extract<keyof TypedEventMap, string>>(
+        type: EventName,
+        ...args: TypedEventMap[EventName] extends void ? [] : [payload: TypedEventMap[EventName]]
+    ): void {
+        const detail = args[0]
         this.target.dispatchEvent(new CustomEvent(type, { detail }))
     }
 }

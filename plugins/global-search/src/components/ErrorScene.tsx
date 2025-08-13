e24840b13f244f440f0fc1116339f83dcfb9138d
Elmar Burke <hi@elmar.codes>
2025-08-13 15:22:18 +0200

    feat(global-search): nicer error boundary

---
+++ plugins/global-search/src/components/ErrorScene.tsx
@@ -0,0 +1,32 @@
+import { framer } from "framer-plugin"
+import { useEffect } from "react"
+import { getPluginSize } from "../utils/plugin-size"
+
+export function ErrorScene() {
+    useEffect(() => {
+        framer.showUI({ ...getPluginSize({ query: "error", hasResults: false }), resizable: false })
+    }, [])
+    return (
+        <div className="flex-1 flex justify-center items-center select-none py-1 px-4">
+            <div className="text-center text-tertiary-light dark:text-tertiary-dark text-xs space-y-4">
+                <p>
+                    The plugin crashed.
+                    <br />
+                    Please close and reopen it.
+                </p>
+                <p>
+                    If it happens again,{" "}
+                    <a
+                        href="https://www.framer.com/contact"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="underline"
+                    >
+                        contact support
+                    </a>
+                    .
+                </p>
+            </div>
+        </div>
+    )
+}

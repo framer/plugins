abd434bedb7c34738dd98d5645abae987a7914cc
Elmar Burke <hi@elmar.codes>
2025-08-27 15:34:28 +0200

    refactor(global-search): highlight results when focused

--- plugins/global-search/src/components/Results.tsx
+++ plugins/global-search/src/components/Results.tsx
@@ -1,6 +1,7 @@
 import { framer } from "framer-plugin"
 import { useCallback, useMemo } from "react"
 import { assertNever } from "../utils/assert"
+import { cn } from "../utils/className"
 import type { EntryResult } from "../utils/filter/group-results"
 import { type Range, rangeLength } from "../utils/filter/ranges"
 import type { RootNodeType } from "../utils/indexer/types"
@@ -52,18 +53,20 @@ function ResultPerEntry({ entry, results }: { entry: EntryResult["entry"]; resul
 
     return (
         <details open className="group flex flex-col not-first:pt-2 not-last:pb-2">
-            <summary className="flex flex-row gap-2 justify-start items-center h-8 select-none overflow-hidden ps-2 pt-2 sticky top-0 bg-modal-light dark:bg-modal-dark">
-                <div className="flex-shrink-0 flex gap-2 justify-start items-center">
-                    <IconArrowRight
-                        className="text-tertiary-light dark:text-tertiary-dark  transition-transform duration-200 ease-in-out group-open:rotate-90"
-                        aria-hidden="true"
-                    />
+            <summary className="pt-2 group focus-visible:outline-none">
+                <div className="flex flex-row gap-2 rounded-lg justify-start items-center h-8 select-none overflow-hidden ps-2 sticky top-0 bg-modal-light dark:bg-modal-dark group-focus-visible:bg-option-light dark:group-focus-visible:bg-option-dark group-focus-visible:text-primary-light dark:group-focus-visible:text-primary-dark">
+                    <div className="flex-shrink-0 flex gap-2 justify-start items-center">
+                        <IconArrowRight
+                            className="text-tertiary-light dark:text-tertiary-dark  transition-transform duration-200 ease-in-out group-open:rotate-90"
+                            aria-hidden="true"
+                        />
 
-                    <ResultIcon rootNodeType={entry.rootNodeType} aria-hidden="true" />
-                </div>
+                        <ResultIcon rootNodeType={entry.rootNodeType} aria-hidden="true" />
+                    </div>
 
-                <div className="text-xs text-secondary-light dark:text-secondary-dark whitespace-nowrap text-ellipsis flex-1 overflow-hidden">
-                    {entry.rootNodeName ?? `Unnamed ${entry.rootNodeType}`}
+                    <div className="text-xs text-secondary-light dark:text-secondary-dark whitespace-nowrap text-ellipsis flex-1 overflow-hidden">
+                        {entry.rootNodeName ?? `Unnamed ${entry.rootNodeType}`}
+                    </div>
                 </div>
             </summary>
             <ul className="flex flex-col">
@@ -107,7 +110,11 @@ function Match({
     return (
         <button
             onClick={navigateToResult}
-            className="text-secondary-light dark:text-secondary-dark text-xs h-6 text-left select-none cursor-pointer pl-5 hover:bg-option-light dark:hover:bg-option-dark hover:text-primary-light dark:hover:text-primary-dark rounded-lg transition-colors"
+            className={cn(
+                "text-secondary-light dark:text-secondary-dark text-xs h-6 text-left select-none cursor-pointer pl-5  rounded-lg transition-colors",
+                "hover:bg-option-light dark:hover:bg-option-dark hover:text-primary-light dark:hover:text-primary-dark focus-visible:bg-option-light dark:focus-visible:bg-option-dark focus-visible:text-primary-light focus-visible:outline-none dark:focus-visible:text-primary-dark",
+                "focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+            )}
         >
             <li className="text-ellipsis overflow-hidden whitespace-nowrap">
                 {before}

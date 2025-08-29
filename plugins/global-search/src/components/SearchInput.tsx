00ccd13c56443880e1f0b77cbdeae76cbf7d2cfa
Elmar Burke <hi@elmar.codes>
2025-08-29 12:37:07 +0200

    fixup! feat(global-search): add arrow up and down navigation for results

--- plugins/global-search/src/components/SearchInput.tsx
+++ plugins/global-search/src/components/SearchInput.tsx
@@ -13,7 +13,6 @@ export function SearchInput({ className, ...props }: SearchInputProps) {
             <span className="sr-only">Search for anything in your Framer project</span>
             <IconSearch aria-hidden className="text-tertiary-light dark:text-tertiary-dark" />
             <input
-                id="text-search-input"
                 type="text"
                 className="flex-1 h-[18px] bg-transparent border-none outline-none focus-visible:outline-none focus-visible:ring-0 text-xs p-0 text-primary-light dark:text-primary-dark placeholder:text-tertiary-light dark:placeholder:text-tertiary-dark"
                 placeholder="Search..."

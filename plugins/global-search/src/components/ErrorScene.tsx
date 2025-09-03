44288feefccea476ffd4107b0a42704160570d98
Elmar Burke <hi@elmar.codes>
2025-09-03 10:21:11 +0200

    fix(global-search): ErrorScene sizing

--- plugins/global-search/src/components/ErrorScene.tsx
+++ plugins/global-search/src/components/ErrorScene.tsx
@@ -4,29 +4,22 @@ import { getPluginUiOptions } from "../utils/plugin-ui"
 
 export function ErrorScene() {
     useEffect(() => {
-        void framer.showUI(getPluginUiOptions({ query: "error", hasResults: false }))
+        void framer.showUI(getPluginUiOptions({ query: "error", hasResults: false, areResultsFinal: true }))
     }, [])
 
     return (
-        <div className="flex-1 flex justify-center items-center select-none py-1 px-4">
+        <div className="flex-1 flex justify-center items-center select-none py-1 px-5">
             <div className="text-center text-tertiary-light dark:text-tertiary-dark text-xs space-y-4">
-                <p>
-                    The plugin crashed.
-                    <br />
-                    Please close and reopen it.
-                </p>
-                <p>
-                    If it happens again,{" "}
-                    <a
-                        href="https://www.framer.com/contact"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="underline"
-                    >
-                        contact support
-                    </a>
-                    .
-                </p>
+                The plugin crashed. Please close and reopen it. If it happens again,{" "}
+                <a
+                    href="https://www.framer.com/contact"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-[#0099FF]"
+                >
+                    contact support
+                </a>
+                .
             </div>
         </div>
     )

3fca2efe96ec0d4df6a0781429d8cca30862ba83
Elmar Burke <hi@elmar.codes>
2025-09-03 13:55:24 +0200

    feat(global-search): add keyboard shortcuts to close the plugin

--- plugins/global-search/src/App.tsx
+++ plugins/global-search/src/App.tsx
@@ -7,6 +7,19 @@ import { SearchScene } from "./components/SearchScene"
 import { IndexerProvider } from "./utils/indexer/IndexerProvider"
 import { getPluginUiOptions } from "./utils/plugin-ui"
 
+// Close the plugin when "open combination" or "escape" is pressed
+document.addEventListener("keydown", event => {
+    const isModifierPressed = event.metaKey || event.ctrlKey
+    const isOwnOpenCombination = isModifierPressed && event.shiftKey && event.code === "KeyF"
+    const isEscape = event.key === "Escape"
+
+    if (!isEscape && !isOwnOpenCombination) return
+
+    // This will close the plugin, but also show a message that the plugin was closed.
+    // We might add a "silent" option later.
+    void framer.closePlugin()
+})
+
 void framer.showUI(getPluginUiOptions({ query: undefined, hasResults: false }))
 
 const projectInfo = await framer.getProjectInfo()

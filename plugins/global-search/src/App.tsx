ce957fe6c227d7880eff615eeac91065ea1818a2
Elmar Burke <hi@elmar.codes>
2025-09-03 13:10:12 +0200

    refactor(global-search): remove DevToolsScene and related functionality

--- plugins/global-search/src/App.tsx
+++ plugins/global-search/src/App.tsx
@@ -1,7 +1,5 @@
 import { framer } from "framer-plugin"
-import { useEffect, useState } from "react"
 import { ErrorBoundary } from "react-error-boundary"
-import { DevToolsScene } from "./components/DevToolsScene"
 import { ErrorScene } from "./components/ErrorScene"
 import { SearchScene } from "./components/SearchScene"
 import { IndexerProvider } from "./utils/indexer/IndexerProvider"
@@ -12,25 +10,10 @@ void framer.showUI(getPluginUiOptions({ query: undefined, hasResults: false }))
 const projectInfo = await framer.getProjectInfo()
 
 export function App() {
-    const [activeScene, setActiveScene] = useState<"search" | "dev-tools">("search")
-
-    useEffect(() => {
-        void framer.setMenu([
-            {
-                label: "Open Dev Tools",
-                onAction: () => {
-                    setActiveScene(state => (state === "dev-tools" ? "search" : "dev-tools"))
-                },
-                checked: activeScene === "dev-tools",
-            },
-        ])
-    })
-
     return (
         <ErrorBoundary FallbackComponent={ErrorScene}>
             <IndexerProvider projectId={projectInfo.id} projectName={projectInfo.name}>
-                {activeScene === "dev-tools" && <DevToolsScene />}
-                {activeScene === "search" && <SearchScene />}
+                <SearchScene />
             </IndexerProvider>
         </ErrorBoundary>
     )

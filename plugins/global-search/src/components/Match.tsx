18bd2569a04d704c4cfc56e9304bfb70b20d2654
Elmar Burke <hi@elmar.codes>
2025-09-10 13:47:38 +0200

    feat(global-search): highlight code file match

---
+++ plugins/global-search/src/components/Match.tsx
@@ -0,0 +1,88 @@
+import { framer } from "framer-plugin"
+import { useCallback, useMemo } from "react"
+import { cn } from "../utils/className"
+import { type Range, rangeLength, rangeToCodeFileLocation } from "../utils/filter/ranges"
+import { ResultType } from "../utils/filter/types"
+import { truncateFromStart } from "../utils/text"
+import { useFocusHandlers } from "../utils/useFocus"
+
+interface CommonMatchProps {
+    targetId: string
+    text: string
+    range: Range
+}
+
+type TypedMatchProps =
+    | {
+          type: ResultType.CollectionItemField
+          collectionFieldId: string
+      }
+    | {
+          type: Exclude<ResultType, ResultType.CollectionItemField>
+          collectionFieldId?: never
+      }
+
+export type MatchProps = CommonMatchProps & TypedMatchProps
+
+export function Match({ targetId, text, range, ...props }: MatchProps) {
+    const navigateToResult = useCallback(() => {
+        framer
+            .navigateTo(targetId, {
+                scrollTo: getScrollToForMatch(range, text, props),
+                zoomIntoView: {
+                    maxZoom: 1,
+                },
+            })
+            .catch((error: unknown) => {
+                framer.notify(`Failed to go to item. ${error instanceof Error ? error.message : "Unknown error"}`)
+            })
+    }, [targetId, range, text, props])
+
+    const { before, highlight, after } = useHighlightedTextWithContext({ text, range })
+    const focusProps = useFocusHandlers({ isSelfSelectable: true })
+
+    return (
+        <button
+            onClick={navigateToResult}
+            className={cn(
+                "text-secondary-light dark:text-secondary-dark text-xs h-6 text-left select-none cursor-pointer pl-5  rounded-lg transition-colors",
+                "hover:bg-option-light/50 dark:hover:bg-option-dark/50 hover:text-primary-light dark:hover:text-primary-dark focus:bg-option-light dark:focus:bg-option-dark focus:text-primary-light dark:focus:text-primary-dark",
+                "focus:outline-none focus:ring-0 focus:ring-offset-0"
+            )}
+            {...focusProps}
+        >
+            <li className="text-ellipsis overflow-hidden whitespace-nowrap">
+                {before}
+                <span className="font-semibold bg-transparent">{highlight}</span>
+                {after}
+            </li>
+        </button>
+    )
+}
+
+function getScrollToForMatch(range: Range, text: string, typedMatchProps: TypedMatchProps) {
+    switch (typedMatchProps.type) {
+        case ResultType.CollectionItemField:
+            return { collectionFieldId: typedMatchProps.collectionFieldId }
+        case ResultType.CodeFile:
+            return { codeFilePosition: rangeToCodeFileLocation(range, text) }
+        default:
+            return undefined
+    }
+}
+
+const rowLength = 30
+function useHighlightedTextWithContext({ text, range }: { text: string; range: Range }) {
+    const [start, end] = range
+    const maxBeforeLength = Math.floor((rowLength - rangeLength(range)) / 2)
+    const before = text.slice(0, start)
+    const highlight = text.slice(start, end)
+    const after = text.slice(end)
+
+    const limitedBefore = useMemo(
+        () => (text.length < rowLength ? before : truncateFromStart(before, maxBeforeLength)),
+        [before, text, maxBeforeLength]
+    )
+
+    return { before: limitedBefore, highlight: highlight, after }
+}

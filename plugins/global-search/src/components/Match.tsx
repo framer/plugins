b9fd009b5ce53e717406779f30fdacf35a10dd00
Elmar Burke <hi@elmar.codes>
2025-09-24 14:16:07 +0200

    fix(global-search): padding on the right side of the result

--- plugins/global-search/src/components/Match.tsx
+++ plugins/global-search/src/components/Match.tsx
@@ -52,7 +52,7 @@ export const Match = forwardRef<HTMLButtonElement, MatchProps>(function Match(pr
             tabIndex={-1}
             onClick={navigateToResult}
             className={cn(
-                "text-secondary-light dark:text-secondary-dark text-xs w-full text-left select-none pl-5 rounded-lg h-6 left-0 scroll-m-8",
+                "text-secondary-light dark:text-secondary-dark text-xs w-full text-left select-none pl-5 pr-1 rounded-lg h-6 left-0 scroll-m-8",
                 "hover:bg-option-light/50 dark:hover:bg-option-dark/50 hover:text-primary-light dark:hover:text-primary-dark",
                 "aria-selected:bg-option-light dark:aria-selected:bg-option-dark aria-selected:text-primary-light dark:aria-selected:text-primary-dark",
                 "focus:outline-none focus:ring-0 focus:ring-offset-0",

7e36b88873409a71f00860cd7d66543c1dd865b7
Clément Roche <rchclement@gmail.com>
2025-10-06 19:31:56 +0200

    conditional renderin plugin

--- plugins/crowdin/src/main.tsx
+++ plugins/crowdin/src/main.tsx
@@ -11,8 +11,18 @@ if (!root) throw new Error("Root element not found")
 const activeLocale = await framer.getActiveLocale()
 const locales = await framer.getLocales()
 
-ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-        <App activeLocale={activeLocale} locales={locales} />
-    </React.StrictMode>
-)
+const isAllowedToSetLocalizationData = framer.isAllowedTo("setLocalizationData")
+
+if (!isAllowedToSetLocalizationData) {
+    framer.notify("You are not allowed to set localization data", { variant: "error" })
+    framer.closePlugin()
+} else if (!activeLocale) {
+    framer.notify("No active locale found", { variant: "error" })
+    framer.closePlugin()
+} else {
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App activeLocale={activeLocale} locales={locales} />
+        </React.StrictMode>
+    )
+}

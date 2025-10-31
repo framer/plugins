14315512812b1762f5d48b655c23d860bebd8998
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-10-31 12:08:03 -0400

    Fix formatting

--- plugins/notion/src/data.ts
+++ plugins/notion/src/data.ts
@@ -117,7 +117,7 @@ export async function syncCollection(
 
             let isUnchanged = false
             if (isUnchangedSinceLastSync(item.last_edited_time, lastSynced)) {
-                if (import.meta.env.MODE  === "development") {
+                if (import.meta.env.MODE === "development") {
                     console.warn({
                         message: `Skipping content update. last updated: ${formatDate(item.last_edited_time)}, last synced: ${lastSynced ? formatDate(lastSynced) : "never"}`,
                         url: item.url,

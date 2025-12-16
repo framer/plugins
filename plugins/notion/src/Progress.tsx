bca105373204ab36c8d175596adb0e56740d8977
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-12-16 11:55:06 -0500

    Use tertiary color

--- plugins/notion/src/Progress.tsx
+++ plugins/notion/src/Progress.tsx
@@ -1,21 +1,23 @@
 export function Progress({ current, total }: { current: number; total: number }) {
     const progressPercent = total > 0 ? ((current / total) * 100).toFixed(1).replace(".0", "") : "0"
-    const formatter = new Intl.NumberFormat('en-US')
+    const formatter = new Intl.NumberFormat("en-US")
     const formattedCurrent = formatter.format(current)
     const formattedTotal = formatter.format(total)
 
     return (
         <main>
             <div className="progress-bar-text">
-                <p>{progressPercent}%</p>
-                <p>
+                <span>{progressPercent}%</span>
+                <span>
                     {formattedCurrent} / {formattedTotal}
-                </p>
+                </span>
             </div>
             <div className="progress-bar">
                 <div className="progress-bar-fill" style={{ width: `${progressPercent}%` }} />
             </div>
-            <p>{current > 0 ? "Syncing" : "Loading data"}… please keep the plugin open until the process is complete.</p>
+            <p>
+                {current > 0 ? "Syncing" : "Loading data"}… please keep the plugin open until the process is complete.
+            </p>
         </main>
     )
 }

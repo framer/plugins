9c742da84a00d30b8b160f5b878a58b87ca9f311
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-12-16 10:01:54 -0500

    Add progress bar modal

---
+++ plugins/notion/src/Progress.tsx
@@ -0,0 +1,19 @@
+export function Progress({ current, total }: { current: number; total: number }) {
+    const progressPercent = total > 0 ? ((current / total) * 100).toFixed(1).replace(".0", "") : "0"
+
+    return (
+        <main>
+            <div className="progress-bar-text">
+                <p>{progressPercent}%</p>
+                <p>
+                    {current} / {total}
+                </p>
+            </div>
+            <div className="progress-bar">
+                <div className="progress-bar-fill" style={{ width: `${progressPercent}%` }} />
+            </div>
+            <p>Syncing... please keep the plugin open until the process is complete.</p>
+            <button>Cancel</button>
+        </main>
+    )
+}

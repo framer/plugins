fb1644107f537586da0c6c8ae2666edf667d6cf4
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-12-16 10:01:54 -0500

    Add progress bar modal

--- plugins/notion/src/FieldMapping.tsx
+++ plugins/notion/src/FieldMapping.tsx
@@ -23,6 +23,7 @@ import {
     type SyncProgress,
     syncCollection,
 } from "./data"
+import { Progress } from "./Progress"
 import { assert, syncMethods } from "./utils"
 
 const labelByFieldTypeOption: Record<ManagedCollectionField["type"], string> = {
@@ -144,6 +145,7 @@ interface FieldMappingProps {
     previousLastSynced: string | null
     previousIgnoredFieldIds: string | null
     databaseIdMap: DatabaseIdMap
+    setIsSyncing: (isSyncing: boolean) => void
 }
 
 export function FieldMapping({
@@ -153,6 +155,7 @@ export function FieldMapping({
     previousLastSynced,
     previousIgnoredFieldIds,
     databaseIdMap,
+    setIsSyncing,
 }: FieldMappingProps) {
     const isAllowedToManage = useIsAllowedTo("ManagedCollection.setFields", ...syncMethods)
 
@@ -250,6 +253,7 @@ export function FieldMapping({
         const task = async () => {
             try {
                 setStatus("syncing-collection")
+                setIsSyncing(true)
                 setSyncProgress(null)
                 await framer.setCloseWarning("Synchronization in progress. Closing will cancel the sync.")
 
@@ -286,6 +290,7 @@ export function FieldMapping({
             } finally {
                 await framer.setCloseWarning(false)
                 setStatus("mapping-fields")
+                setIsSyncing(false)
                 setSyncProgress(null)
             }
         }
@@ -301,7 +306,9 @@ export function FieldMapping({
         )
     }
 
-    const progressPercent = syncProgress ? ((syncProgress.current / syncProgress.total) * 100).toFixed(1) : null
+    if (isSyncing) {
+        return <Progress current={syncProgress?.current ?? 0} total={syncProgress?.total ?? 0} />
+    }
 
     return (
         <main className="framer-hide-scrollbar mapping">
@@ -356,11 +363,7 @@ export function FieldMapping({
                         tabIndex={0}
                         title={!isAllowedToManage ? "Insufficient permissions" : undefined}
                     >
-                        {isSyncing ? (
-                            <>{!syncProgress ? <div className="framer-spinner" /> : <span>{progressPercent}%</span>}</>
-                        ) : (
-                            <span>Import from {dataSourceName.trim() ? dataSourceName : "Untitled"}</span>
-                        )}
+                        <span>Import from {dataSourceName.trim() ? dataSourceName : "Untitled"}</span>
                     </button>
                 </footer>
             </form>

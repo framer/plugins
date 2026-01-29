f79a7972dc284710aa168624f3a021d6ca5f6e32
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-01-29 00:42:03 -0500

    Move to react component

--- plugins/notion/src/main.tsx
+++ plugins/notion/src/main.tsx
@@ -7,14 +7,8 @@ import ReactDOM from "react-dom/client"
 import { App } from "./App"
 import { PLUGIN_KEYS } from "./api"
 import auth from "./auth"
-import {
-    getExistingCollectionDatabaseIdMap,
-    type SyncProgress,
-    shouldSyncExistingCollection,
-    syncExistingCollection,
-} from "./data"
+import { getExistingCollectionDatabaseIdMap } from "./data"
 import { Authenticate } from "./Login"
-import { ProgressApp } from "./ProgressApp"
 
 const activeCollection = await framer.getActiveManagedCollection()
 
@@ -53,57 +47,16 @@ const [
     getExistingCollectionDatabaseIdMap(),
 ])
 
-const shouldSync = shouldSyncExistingCollection({ previousSlugFieldId, previousDatabaseId })
-
-let didSync = false
-
-if (shouldSync) {
-    // Render progress UI component
-    const reactRoot = ReactDOM.createRoot(root)
-
-    const setSyncProgress = await new Promise<(progress: SyncProgress) => void>(resolve => {
-        reactRoot.render(
-            <React.StrictMode>
-                <ProgressApp
-                    onProgressReady={(setter: (progress: SyncProgress) => void) => {
-                        resolve(setter)
-                    }}
-                />
-            </React.StrictMode>
-        )
-    })
-
-    const { didSync: didSyncResult } = await syncExistingCollection(
-        activeCollection,
-        previousDatabaseId,
-        previousSlugFieldId,
-        previousIgnoredFieldIds,
-        previousLastSynced,
-        previousDatabaseName,
-        existingCollectionDatabaseIdMap,
-        setSyncProgress
-    )
-
-    didSync = didSyncResult
-    reactRoot.unmount()
-}
-
-if (didSync) {
-    framer.closePlugin("Synchronization successful", {
-        variant: "success",
-    })
-} else {
-    ReactDOM.createRoot(root).render(
-        <React.StrictMode>
-            <App
-                collection={activeCollection}
-                previousDatabaseId={previousDatabaseId}
-                previousSlugFieldId={previousSlugFieldId}
-                previousLastSynced={previousLastSynced}
-                previousIgnoredFieldIds={previousIgnoredFieldIds}
-                previousDatabaseName={previousDatabaseName}
-                existingCollectionDatabaseIdMap={existingCollectionDatabaseIdMap}
-            />
-        </React.StrictMode>
-    )
-}
+ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+        <App
+            collection={activeCollection}
+            previousDatabaseId={previousDatabaseId}
+            previousSlugFieldId={previousSlugFieldId}
+            previousLastSynced={previousLastSynced}
+            previousIgnoredFieldIds={previousIgnoredFieldIds}
+            previousDatabaseName={previousDatabaseName}
+            existingCollectionDatabaseIdMap={existingCollectionDatabaseIdMap}
+        />
+    </React.StrictMode>
+)

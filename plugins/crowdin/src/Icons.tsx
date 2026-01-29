5a72bb435560f68e5720400bda4b86dd88243314
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-01-29 12:44:03 -0500

    Move icons

---
+++ plugins/crowdin/src/Icons.tsx
@@ -0,0 +1,46 @@
+export function XIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8.5">
+            <g fill="transparent" strokeWidth="1.5" stroke="currentColor" strokeLinecap="round">
+                <path d="m1.5 6.75 5-5M6.5 6.75l-5-5"></path>
+            </g>
+        </svg>
+    )
+}
+
+export function ChevronDownIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8">
+            <path
+                d="m1 2.75 3 3 3-3"
+                fill="transparent"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            ></path>
+        </svg>
+    )
+}
+
+export function CheckIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="8"
+            height="4.5"
+            viewBox="0 0 6 4.5"
+            fill="none"
+            overflow="visible"
+        >
+            <path
+                d="M 0 2.5 L 2.118 4.5 L 6 0"
+                fill="transparent"
+                strokeWidth="1.5"
+                stroke="rgb(64, 222, 127)"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            ></path>
+        </svg>
+    )
+}

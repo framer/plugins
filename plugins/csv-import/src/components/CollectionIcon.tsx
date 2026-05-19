979255b949e024e062ee59798ae768c61139586b
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-05-19 11:52:50 -0400

    CSV Import prerelease styles

--- plugins/csv-import/src/components/CollectionIcon.tsx
+++ plugins/csv-import/src/components/CollectionIcon.tsx
@@ -1,5 +1,33 @@
+import { useMemo } from "react"
+
 export function CollectionIcon() {
-    return (
+    const isPrerelease = useMemo(() => document.body.getAttribute("data-framer-styles") === "prerelease", [])
+
+    return isPrerelease ? (
+        <svg
+            role="presentation"
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            aria-hidden="true"
+            focusable="false"
+        >
+            <path
+                fill="currentColor"
+                fillOpacity="0.2"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                d="M1.5 8.75v-5.5C1.5 1.869 3.515.75 6 .75s4.5 1.119 4.5 2.5v5.5m0 0c0 1.381-2.015 2.5-4.5 2.5s-4.5-1.119-4.5-2.5"
+            />
+            <path
+                fill="none"
+                stroke="currentColor"
+                d="M10.25 3.25c0 1.105-1.903 2-4.25 2s-4.25-.895-4.25-2M10.25 6c0 1.105-1.903 2-4.25 2s-4.25-.895-4.25-2"
+            />
+        </svg>
+    ) : (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width={12}

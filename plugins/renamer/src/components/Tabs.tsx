6449da6001fbe536a61198c8e7684f1cf9181503
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-05-20 10:25:35 -0400

    Use selector for tabs

--- plugins/renamer/src/components/Tabs.tsx
+++ plugins/renamer/src/components/Tabs.tsx
@@ -15,30 +15,22 @@ export default function Tabs({ items }: Props) {
     const isPrerelease = useMemo(() => document.body.getAttribute("data-framer-styles") === "prerelease", [])
     const activeIndex = items.findIndex(item => item.active)
 
-    return isPrerelease ? (
-        <div className="prerelease-tabs">
-            <div className="prerelease-tabs-container">
-                {items.map((item, index) => (
-                    <button key={index} className={cx("prerelease-tab", item.active && "active")} onClick={item.select}>
-                        {item.label}
-                    </button>
-                ))}
-            </div>
-        </div>
-    ) : (
+    return (
         <div className="tabs">
             <div className="tabs-container">
-                <div className="tab-bg-container">
-                    {activeIndex !== -1 && (
-                        <div
-                            className="tab-bg"
-                            style={{
-                                width: `${100 / items.length}%`,
-                                left: `${(100 / items.length) * activeIndex}%`,
-                            }}
-                        />
-                    )}
-                </div>
+                {!isPrerelease && (
+                    <div className="tab-bg-container">
+                        {activeIndex !== -1 && (
+                            <div
+                                className="tab-bg"
+                                style={{
+                                    width: `${100 / items.length}%`,
+                                    left: `${(100 / items.length) * activeIndex}%`,
+                                }}
+                            />
+                        )}
+                    </div>
+                )}
 
                 {items.map((item, index) => (
                     <button key={index} className={cx("tab", item.active && "active")} onClick={item.select}>

551ddd6f70101389a7f81b1af1688e2333cdeb23
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-06-15 19:13:36 -0400

    Renamer

--- plugins/renamer/src/components/Tabs.tsx
+++ plugins/renamer/src/components/Tabs.tsx
@@ -1,5 +1,4 @@
 import cx from "classnames"
-import { useMemo } from "react"
 
 interface TabItem {
     label: string
@@ -12,26 +11,9 @@ interface Props {
 }
 
 export default function Tabs({ items }: Props) {
-    const isPrerelease = useMemo(() => document.body.getAttribute("data-framer-styles") === "prerelease", [])
-    const activeIndex = items.findIndex(item => item.active)
-
     return (
         <div className="tabs">
             <div className="tabs-container">
-                {!isPrerelease && (
-                    <div className="tab-bg-container">
-                        {activeIndex !== -1 && (
-                            <div
-                                className="tab-bg"
-                                style={{
-                                    width: `${100 / items.length}%`,
-                                    left: `${(100 / items.length) * activeIndex}%`,
-                                }}
-                            />
-                        )}
-                    </div>
-                )}
-
                 {items.map((item, index) => (
                     <button key={index} className={cx("tab", item.active && "active")} onClick={item.select}>
                         {item.label}

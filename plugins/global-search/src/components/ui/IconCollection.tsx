b61fc14aa4ee829406be69ebd20be60b2d933af6
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-06-15 19:15:31 -0400

    Text search

--- plugins/global-search/src/components/ui/IconCollection.tsx
+++ plugins/global-search/src/components/ui/IconCollection.tsx
@@ -1,9 +1,7 @@
 import { type SVGProps, useMemo } from "react"
 
 export function IconCollection(props: SVGProps<SVGSVGElement>) {
-    const isPrerelease = useMemo(() => document.body.getAttribute("data-framer-styles") === "prerelease", [])
-
-    return isPrerelease ? (
+    return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="12"
@@ -25,30 +23,6 @@ export function IconCollection(props: SVGProps<SVGSVGElement>) {
                 fill="none"
                 stroke="currentColor"
                 d="M10.25 3.25c0 1.105-1.903 2-4.25 2s-4.25-.895-4.25-2M10.25 6c0 1.105-1.903 2-4.25 2s-4.25-.895-4.25-2"
-            />
-        </svg>
-    ) : (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={12}
-            height={12}
-            viewBox="0 0 12 12"
-            fill="none"
-            role="presentation"
-            aria-hidden="true"
-            {...props}
-        >
-            <path
-                d="M 6 0.5 C 8.761 0.5 11 1.619 11 3 C 11 4.381 8.761 5.5 6 5.5 C 3.239 5.5 1 4.381 1 3 C 1 1.619 3.239 0.5 6 0.5 Z"
-                fill="currentColor"
-            />
-            <path
-                d="M 11 6 C 11 7.381 8.761 8.5 6 8.5 C 3.239 8.5 1 7.381 1 6 C 1 5.711 1.098 5.433 1.278 5.175 C 2.377 5.991 4.084 6.515 6 6.515 C 7.916 6.515 9.623 5.991 10.722 5.175 C 10.902 5.433 11 5.711 11 6 Z"
-                fill="currentColor"
-            />
-            <path
-                d="M 11 9 C 11 10.381 8.761 11.5 6 11.5 C 3.239 11.5 1 10.381 1 9 C 1 8.711 1.098 8.433 1.278 8.175 C 2.377 8.991 4.084 9.515 6 9.515 C 7.916 9.515 9.623 8.991 10.722 8.175 C 10.902 8.433 11 8.711 11 9 Z"
-                fill="currentColor"
             />
         </svg>
     )
--- plugins/global-search/src/components/ui/IconComponent.tsx
+++ plugins/global-search/src/components/ui/IconComponent.tsx
@@ -1,9 +1,7 @@
 import { type SVGProps, useMemo } from "react"
 
 export function IconComponent(props: SVGProps<SVGSVGElement>) {
-    const isPrerelease = useMemo(() => document.body.getAttribute("data-framer-styles") === "prerelease", [])
-
-    return isPrerelease ? (
+    return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="12"
@@ -22,13 +20,6 @@ export function IconComponent(props: SVGProps<SVGSVGElement>) {
                 d="M4.586 1.354a2 2 0 0 1 2.828 0l3.232 3.232a2 2 0 0 1 0 2.828l-3.232 3.232a2 2 0 0 1-2.828 0L1.354 7.414a2 2 0 0 1 0-2.828Z"
             />
             <path fill="none" stroke="currentColor" d="m3 3 6 6M9 3 3 9" />
-        </svg>
-    ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" width={11} height={11} fill="none" {...props}>
-            <path
-                fill="currentColor"
-                d="M4.4.6C5 0 6 0 6.6.6L8.2 2 5.5 4.8 2.8 2zM2.1 8.2L.6 6.6C0 6 0 5 .6 4.4L2 2.8l2.7 2.7zm8.3-3.8c.6.6.6 1.6 0 2.2L9 8.2 6.2 5.5 9 2.8zm-3.8 6c-.6.6-1.6.6-2.2 0L2.8 9l2.7-2.7L8.2 9z"
-            />
         </svg>
     )
 }
